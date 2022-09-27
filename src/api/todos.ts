8ca048f614707180f1acc53276016646bcef4dad
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-09-27 09:25:27 +0300

    First commit

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,14 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const updateTodo = (todoId: number, completed: boolean) => {
+  return client.patch<Todo>(`/todos/${todoId}`, { completed });
+};
+
+export const addTodo = (data: Todo) => {
+  return client.post<Todo>('/todos', data);
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
