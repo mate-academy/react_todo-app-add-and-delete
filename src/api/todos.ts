095bb64100fff887f11b823deb0e328f015575ec
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-09-29 20:28:56 +0300

    Methods POST and DELETE was created

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,14 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const postTodo = (userId: number, title: string) => {
+  return client.post<Todo>('/todos', {
+    userId,
+    title,
+    completed: false,
+  });
+};
+
+export const deleteTodo = (userId: number) => {
+  return client.delete(`/todo/${userId}`);
+};
