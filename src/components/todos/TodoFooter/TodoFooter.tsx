0ec745cf80088fd4be36b383dbd5322a361ba74b
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-09-30 15:06:27 +0300

    components was added

---
+++ src/components/todos/TodoFooter/TodoFooter.tsx
@@ -0,0 +1,47 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Todo } from '../../../types/Todo';
+
+interface Props {
+  todos: Todo[];
+  selected: string;
+  setStatus: (status: string) => void;
+}
+
+export const TodoFooter: React.FC<Props> = ({ todos, selected, setStatus }) => {
+  const statuses = ['All', 'Active', 'Completed'];
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="todosCounter">
+        {todos.filter(todo => !todo.completed).length}
+        {' items left'}
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        {statuses.map(status => (
+          <a
+            key={status}
+            data-cy="FilterLinkAll"
+            href="#/"
+            className={classNames('filter__link', {
+              selected: selected === status,
+            })}
+            onClick={() => setStatus(status)}
+          >
+            {status}
+          </a>
+        ))}
+
+      </nav>
+
+      <button
+        data-cy="ClearCompletedButton"
+        type="button"
+        className="todoapp__clear-completed"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
---
+++ src/components/todos/TodoHeader/TodoHeader.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useEffect, useRef } from 'react';
+
+export const TodoHeader: React.FC = () => {
+  const newTodoField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // focus the element with `ref={newTodoField}`
+    if (newTodoField.current) {
+      newTodoField.current.focus();
+    }
+  }, []);
+
+  return (
+    <header className="todoapp__header">
+      <button
+        data-cy="ToggleAllButton"
+        type="button"
+        className="todoapp__toggle-all active"
+      />
+
+      <form>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          ref={newTodoField}
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
