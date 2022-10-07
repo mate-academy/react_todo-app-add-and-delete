10cdd4be40f67c401c6606e70e88801c73a6e93e
Andrii Dunets <davbiznesman@gmail.com>
2022-10-07 16:58:12 +0300

    react todo app add and delete task done

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -6,3 +6,15 @@ export const getTodos = (userId: number) => {
 };
 
 // Add more methods here
+
+export const createTodo = (userId: number, title: string) => {
+  return client.post<Todo>('/todos', {
+    userId,
+    title,
+    completed: false,
+  });
+};
+
+export const removeTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
