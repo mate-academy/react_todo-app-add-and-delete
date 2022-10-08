7936677da66f0f959d21577bde731ceb23308b3e
Oleksii <oleksii@it.com>
2022-10-08 19:28:17 +0300

    Need help

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,15 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const createTodos = (title: string, userId: number) => {
+  return client.post<Todo>('/todos',
+    {
+      title,
+      userId,
+      completed: false,
+    });
+};
+
+export const deleteTodos = (todoId: number) => {
+  return client.delete(`./todos/${todoId}`);
+};
