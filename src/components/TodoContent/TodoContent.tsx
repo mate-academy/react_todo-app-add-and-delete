0ff06be1f5a8942de8dd2ab75e068e904ffc6719
Killdex <kdcompany2018@gmail.com>
2022-11-07 14:31:32 +0200

    added all files

---
+++ src/components/TodoContent/TodoContent.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { TodoHeader } from '../TodoHeader';
+import { TodoBody } from '../TodoBody';
+import { TodoFooter } from '../TodoFooter';
+
+import { Todo } from '../../types/Todo';
+import { FilterStatus } from '../../types/FilterStatus';
+
+type Props = {
+  newTodoField: React.RefObject<HTMLInputElement>;
+  todos: Todo[],
+  visibleTodos: Todo[],
+  filterTodos: (filterBy: FilterStatus) => void;
+  filterStatus: FilterStatus,
+  onChangeTitle: (title: string) => void;
+  todoTitle: string;
+  createNewTodo: (title: string) => void;
+  isLoading: boolean;
+  deleteTodo: (todoId: number) => void;
+  deletedTodoId: number[],
+  onCompleteTodo: (todoId: number) => void;
+  deleteAllCompletedTodos: () => void;
+};
+
+export const TodoContent: React.FC<Props> = React.memo(({
+  newTodoField,
+  todos,
+  visibleTodos,
+  filterTodos,
+  filterStatus,
+  onChangeTitle,
+  todoTitle,
+  createNewTodo,
+  isLoading,
+  deleteTodo,
+  deletedTodoId,
+  onCompleteTodo,
+  deleteAllCompletedTodos,
+}) => {
+  const countOfTodos = todos.length;
+  const countOfLeftTodos = todos.filter(todo => !todo.completed).length;
+
+  return (
+    <div className="todoapp__content">
+      <TodoHeader
+        countOfTodos={countOfTodos}
+        newTodoField={newTodoField}
+        onChangeTitle={onChangeTitle}
+        todoTitle={todoTitle}
+        createNewTodo={createNewTodo}
+        isAdding={isLoading}
+      />
+
+      {
+        todos.length > 0
+        && (
+          <>
+            <TodoBody
+              visibleTodos={visibleTodos}
+              isLoading={isLoading}
+              todoTitle={todoTitle}
+              deleteTodo={deleteTodo}
+              deletedTodoId={deletedTodoId}
+              onCompleteTodo={onCompleteTodo}
+            />
+
+            <TodoFooter
+              filterTodos={filterTodos}
+              filterStatus={filterStatus}
+              countOfLeftTodos={countOfLeftTodos}
+              deleteAllCompletedTodos={deleteAllCompletedTodos}
+            />
+          </>
+        )
+      }
+    </div>
+  );
+});
