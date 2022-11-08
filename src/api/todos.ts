7f9a3e8b16d41cf1d8602dbd0d84dd392eb0281c
Serhii Hluhovskyi <glugovsky@proton.me>
2022-11-09 00:05:41 +0200

    Implemented todo app add and delete solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,10 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const addTodo = (newTodo: Partial<Todo>) => (
+  client.post<Todo>('/todos', newTodo)
+);
+
+export const removeTodo = (todoId: number) => (
+  client.delete(`/todos/${todoId}`)
+);
