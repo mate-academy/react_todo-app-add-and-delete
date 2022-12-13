4fa0e1bbefb2359481ea0227afa371681eab953b
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-12-14 00:20:53 +0200

    fixed

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -10,113 +10,108 @@ type Props = {
   onComplete: (todo: Todo) => void,
 };
 
-export const TodoItem: React.FC<Props> = React.memo(
-  ({
-    todo,
-    isProcessing,
-    onDelete,
-    onRename,
-    onComplete,
-  }) => {
-    const [isRenaming, setIsRenaming] = useState(false);
-    const [title, setTitle] = useState(todo.title);
-    const [newTitle, setNewTitle] = useState(todo.title);
-    const renameInput = useRef<HTMLInputElement>(null);
+export const TodoItem: React.FC<Props> = React.memo(({
+  todo,
+  isProcessing,
+  onDelete,
+  onRename,
+  onComplete,
+}) => {
+  const [isRenaming, setIsRenaming] = useState(false);
+  const [title, setTitle] = useState(todo.title);
+  const [newTitle, setNewTitle] = useState(todo.title);
+  const renameInput = useRef<HTMLInputElement>(null);
 
-    const onRenameHander = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
+  const handleRename = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!newTitle) {
+      onDelete(todo.id);
 
-      if (!newTitle) {
-        onDelete(todo.id);
+      return;
+    }
 
-        return;
-      }
-
-      if (title === newTitle) {
-        setIsRenaming(false);
-
-        return;
-      }
-
+    if (title === newTitle) {
       setIsRenaming(false);
-      setTitle(newTitle);
-      onRename(todo.id, newTitle);
-    };
 
-    useEffect(() => {
-      renameInput.current?.focus();
+      return;
+    }
 
-      document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-          setIsRenaming(false);
-        }
-      });
-    }, [isRenaming]);
+    setIsRenaming(false);
+    setTitle(newTitle);
+    onRename(todo.id, newTitle);
+  };
 
-    return (
+  useEffect(() => {
+    renameInput.current?.focus();
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') {
+        setIsRenaming(false);
+      }
+    });
+  }, [isRenaming]);
+
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames('todo', {
+        completed: todo.completed,
+      })}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          defaultChecked={todo.completed}
+          onClick={() => onComplete(todo)}
+        />
+      </label>
+
+      {!isRenaming
+        ? (
+          <>
+            <span
+              data-cy="TodoTitle"
+              className="todo__title"
+              onDoubleClick={() => setIsRenaming(true)}
+            >
+              {title}
+            </span>
+            <button
+              type="button"
+              className="todo__remove"
+              data-cy="TodoDeleteButton"
+              onClick={() => onDelete(todo.id)}
+            >
+              ×
+            </button>
+          </>
+        ) : (
+          <form
+            onSubmit={handleRename}
+            onBlur={handleRename}
+          >
+            <input
+              data-cy="TodoTitleField"
+              ref={renameInput}
+              type="text"
+              className="todo__title-field"
+              placeholder="Empty todo will be deleted"
+              value={newTitle}
+              onChange={(event) => setNewTitle(event.target.value)}
+            />
+          </form>
+        )}
       <div
-        data-cy="Todo"
-        className={classNames('todo', {
-          completed: todo.completed,
+        data-cy="TodoLoader"
+        className={classNames('modal overlay', {
+          'is-active': isProcessing,
         })}
       >
-        <label className="todo__status-label">
-          <input
-            data-cy="TodoStatus"
-            type="checkbox"
-            className="todo__status"
-            defaultChecked={todo.completed}
-            onClick={() => onComplete(todo)}
-          />
-        </label>
-
-        {!isRenaming
-          ? (
-            <>
-              <span
-                data-cy="TodoTitle"
-                className="todo__title"
-                onDoubleClick={() => setIsRenaming(true)}
-              >
-                {title}
-              </span>
-
-              <button
-                type="button"
-                className="todo__remove"
-                data-cy="TodoDeleteButton"
-                onClick={() => onDelete(todo.id)}
-              >
-                ×
-              </button>
-            </>
-          ) : (
-            <form
-              onSubmit={onRenameHander}
-              onBlur={onRenameHander}
-            >
-              <input
-                data-cy="TodoTitleField"
-                ref={renameInput}
-                type="text"
-                className="todo__title-field"
-                placeholder="Empty todo will be deleted"
-                value={newTitle}
-                onChange={(event) => setNewTitle(event.target.value)}
-              />
-            </form>
-          )}
-
-        <div
-          data-cy="TodoLoader"
-          className={classNames('modal overlay', {
-            'is-active': isProcessing,
-          })}
-        >
-          <div className="modal-background has-background-white-ter" />
-          <div className="loader" />
-        </div>
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
       </div>
-    );
-  },
-);
+    </div>
+  );
+});
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -14,46 +14,43 @@ type Props = {
   onComplete: (todo: Todo) => void,
 };
 
-export const TodoList: React.FC<Props> = React.memo(
-  ({
-    visibleTodos,
-    isAdding,
-    newTodoTitle,
-    onDelete, onRename,
-    isProcessing,
-    onComplete,
-  }) => {
-    return (
-      <section className="todoapp__main" data-cy="TodoList">
-        <TransitionGroup component={null}>
-          {visibleTodos.map(todo => (
-            <CSSTransition
-              key={todo.id}
-              classNames="item"
-              timeout={300}
-            >
-              <TodoItem
-                key={todo.id}
-                todo={todo}
-                isProcessing={isProcessing.includes(todo.id)}
-                onDelete={onDelete}
-                onRename={onRename}
-                onComplete={onComplete}
-              />
-            </CSSTransition>
-          ))}
+export const TodoList: React.FC<Props> = React.memo(({
+  visibleTodos,
+  isAdding,
+  newTodoTitle,
+  onDelete, onRename,
+  isProcessing,
+  onComplete,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      <TransitionGroup component={null}>
+        {visibleTodos.map(todo => (
+          <CSSTransition
+            key={todo.id}
+            classNames="item"
+            timeout={300}
+          >
+            <TodoItem
+              todo={todo}
+              isProcessing={isProcessing.includes(todo.id)}
+              onDelete={onDelete}
+              onRename={onRename}
+              onComplete={onComplete}
+            />
+          </CSSTransition>
+        ))}
 
-          {isAdding && (
-            <CSSTransition
-              key={0}
-              classNames="item"
-              timeout={300}
-            >
-              <TempTodoItem newTodoTitle={newTodoTitle} />
-            </CSSTransition>
-          )}
-        </TransitionGroup>
-      </section>
-    );
-  },
-);
+        {isAdding && (
+          <CSSTransition
+            key={0}
+            classNames="item"
+            timeout={300}
+          >
+            <TempTodoItem newTodoTitle={newTodoTitle} />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+    </section>
+  );
+});
