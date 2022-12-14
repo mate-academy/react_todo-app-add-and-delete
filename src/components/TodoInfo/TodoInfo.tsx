55240e65e6d681531b7dcb1fb0fc3ddb87eea68f
Illia Skladnik <scladnik@gmail.com>
2022-12-14 15:02:22 +0200

    fix

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -77,11 +77,6 @@ export const TodoInfo: React.FC<Props> = (props) => {
         >
           ×
         </button>
-
-        {/* <div data-cy="TodoLoader" className="modal overlay">
-          <div className="modal-background has-background-white-ter" />
-          <div className="loader" />
-        </div> */}
       </div>
     </>
   );
