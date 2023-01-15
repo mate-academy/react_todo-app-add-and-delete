7d9af7831b3ac8881885cb25e324c5b5c6238154
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-01-15 15:04:34 +0200

    solution update 2

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,8 +5,6 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
-
 export const addTodos = (
   todo: Pick<Todo, 'userId' | 'title' | 'completed'>,
 ) => {
