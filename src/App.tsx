275cac2d041196dccaf73ddb91ea85c12f49c3a4
Anastasiia Shyptia <shyptianastya27@gmail.com>
2023-01-19 09:04:41 +0200

    Fix behavoiur when there is no todos and you add the first todo

--- src/App.tsx
+++ src/App.tsx
@@ -178,7 +178,7 @@ export const App: React.FC = () => {
         onSubmitForm={handleSubmitForm}
       />
 
-      {todos.length > 0 && (
+      {(todos.length > 0 || tempTodo) && (
         <>
           <TodoList
             todos={filteredTodos}
