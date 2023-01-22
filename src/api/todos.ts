28fedf3641e179bd80b8266eb2d66e1b1880ed1a
ysrckr <dyt.yasarcakir@gmail.com>
2023-01-22 23:45:45 +0300

    create delete call

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -11,4 +11,8 @@ export const postTodos = (newTodo: TodoData) => {
   return client.post<Todo>('/todos', newTodo);
 };
 
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
 // Add more methods here
