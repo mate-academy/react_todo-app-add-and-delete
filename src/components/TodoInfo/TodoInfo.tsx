8eae4b2d273170800d88aceba9222682aad4acc9
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-01-25 12:52:27 +0200

    not perfect solution

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,52 @@
+import { memo } from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo,
+  onDeleteTodo: (todoId: number) => void,
+  isLoading:boolean
+};
+
+export const TodoInfo: React.FC <Props> = memo(({
+  todo,
+  onDeleteTodo,
+  isLoading,
+}) => {
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames('todo', { completed: todo.completed })}
+      key={todo.id}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          defaultChecked
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">{todo.title}</span>
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDeleteButton"
+        onClick={() => onDeleteTodo(todo.id)}
+      >
+        ×
+      </button>
+
+      <div
+        data-cy="TodoLoader"
+        className={classNames('modal overlay', {
+          'is-active': isLoading,
+        })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+});
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,27 @@
+import { memo } from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+type Props = {
+  todos: Todo[],
+  onDeleteTodo: (todoId: number) => void
+  isLoading:boolean
+};
+
+export const TodoList: React.FC<Props> = memo(({
+  todos,
+  onDeleteTodo,
+  isLoading,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <TodoInfo
+          todo={todo}
+          onDeleteTodo={onDeleteTodo}
+          isLoading={isLoading}
+        />
+      ))}
+    </section>
+  );
+});
