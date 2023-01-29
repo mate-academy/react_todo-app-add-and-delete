083ab1bffcbd11fafaa5fd6753a9563af67fdf49
kreisberg <anastasia.kreisberg@yahoo.com>
2023-01-29 23:04:38 +0200

    error with empty title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 type HeaderProps = {
   newTodoField: React.RefObject<HTMLInputElement>;
+  showError: (message: string) => void
 };
 
-export const Header: React.FC<HeaderProps> = ({ newTodoField }) => (
-  <header className="todoapp__header">
-    {/* eslint-disable-next-line */}
-    <button
-      data-cy="ToggleAllButton"
-      type="button"
-      className="todoapp__toggle-all active"
-    />
+export const Header: React.FC<HeaderProps> = ({ newTodoField, showError }) => {
+  const [title, setTitle] = useState('');
 
-    <form>
-      <input
-        data-cy="NewTodoField"
-        type="text"
-        ref={newTodoField}
-        className="todoapp__new-todo"
-        placeholder="What needs to be done?"
+  const onSubmitForm = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!title) {
+      showError('Title is required');
+    }
+  };
+
+  return (
+    <header className="todoapp__header">
+      {/* eslint-disable-next-line */}
+      <button
+        data-cy="ToggleAllButton"
+        type="button"
+        className="todoapp__toggle-all active"
       />
-    </form>
-  </header>
-);
+
+      <form onSubmit={onSubmitForm}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          ref={newTodoField}
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
