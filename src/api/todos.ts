6acf66239e3a76ae56a078aff1cf460b6c0bb905
TetKopot <tet.kopot@gmail.com>
2023-01-30 02:47:55 +0200

    Solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,17 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const getTodos = (userId: number) => {
+const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
+};
+
+const addTodo = (fieldsToCreate: Omit<Todo, 'id'>) => {
+  return client.post<Todo[]>('/todos', fieldsToCreate);
+};
+
+export const todoApi = {
+  getTodos,
+  addTodo,
 };
 
 // Add more methods here
