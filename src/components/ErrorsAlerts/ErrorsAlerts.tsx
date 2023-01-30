cae070408300090815c62db971f66f9891588ff2
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2023-01-30 16:54:48 +0200

    added React.memo to ErrorsAlerts

--- src/components/ErrorsAlerts/ErrorsAlerts.tsx
+++ src/components/ErrorsAlerts/ErrorsAlerts.tsx
@@ -6,29 +6,31 @@ type Props = {
   closeErrors: (message: string) => void;
 };
 
-export const ErrorsAlerts: React.FC<Props> = ({
-  errorMessage,
-  closeErrors,
-}) => {
-  return (
-    <div
-      data-cy="ErrorNotification"
-      className={cn(
-        'notification is-danger is-light has-text-weight-normal',
-        {
-          hidden: !errorMessage,
-        },
-      )}
-    >
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="HideErrorButton"
-        type="button"
-        className="delete"
-        onClick={() => closeErrors('')}
-      />
+export const ErrorsAlerts: React.FC<Props> = React.memo(
+  ({
+    errorMessage,
+    closeErrors,
+  }) => {
+    return (
+      <div
+        data-cy="ErrorNotification"
+        className={cn(
+          'notification is-danger is-light has-text-weight-normal',
+          {
+            hidden: !errorMessage,
+          },
+        )}
+      >
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={() => closeErrors('')}
+        />
 
-      {errorMessage}
-    </div>
-  );
-};
+        {errorMessage}
+      </div>
+    );
+  },
+);
