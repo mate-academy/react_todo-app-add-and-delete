8f3631e3c764e8d55bd64a70f4f39b6d3e2fbcdc
Vitalii Derlemenko <vitaliidox@gmail.com>
2023-02-02 16:08:49 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,107 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+/* eslint-disable curly */
 
-const USER_ID = 0;
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from './components/Auth/AuthContext';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { ErrorNotification } from './components/ErrorNotification';
+import { TodosProvider } from './components/TodosContext';
+
+import { getTodos } from './api/todos';
+
+import { Todo } from './types/Todo';
+import { Error } from './types/Error';
+import { Filter } from './types/Filter';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [isError, setIsError] = useState<Error | null>(null);
+  const [filter, setFilter] = useState<Filter>(Filter.All);
+  const [isTemp] = useState<Todo[] | null>([{
+    id: 0,
+    userId: 0,
+    title: '',
+    completed: false,
+  }]);
+
+  const user = useContext(AuthContext);
+
+  const setTodosList = () => {
+    if (!user) {
+      return;
+    }
+
+    setTodos(isTemp);
+    getTodos(user.id)
+      .then(data => setTodos(data))
+      .catch(() => {
+        setIsError(Error.Update);
+        setTodos(null);
+      });
+  };
+
+  const filteredTodos = () => {
+    if (!todos) return null;
+
+    return todos?.filter((todo) => {
+      switch (filter) {
+        case Filter.Active: return !todo.completed;
+        case Filter.Completed: return todo.completed;
+        case Filter.All:
+        default:
+          return true;
+      }
+    });
+  };
+
+  const setFilterStatus = (arg: Filter) => {
+    setFilter(arg);
+  };
+
+  const setErrorsArgument = (argument: Error | null) => {
+    setIsError(null);
+    setIsError(argument);
+  };
+
+  useEffect(() => {
+    setTodosList();
+  }, []);
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <TodosProvider
+      setErrorsArgument={setErrorsArgument}
+      setTodos={setTodos}
+      todos={todos}
+      setTodosList={setTodosList}
+    >
+      <div className="todoapp">
+        <h1 className="todoapp__title">todos</h1>
+        <div className="todoapp__content">
+          <Header
+            setErrorsArgument={setErrorsArgument}
+            setTodosList={setTodos}
+            todos={todos}
+          />
+          {todos && (
+            <TodoList todos={filteredTodos()} />
+          )}
+          <Footer
+            filter={filter}
+            setFilter={setFilterStatus}
+            todos={todos}
+            setErrorsArgument={setErrorsArgument}
+            setTodos={setTodos}
+          />
+        </div>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+        {isError && (
+          <ErrorNotification
+            error={isError}
+            setIsError={setIsError}
+          />
+        )}
+      </div>
+    </TodosProvider>
   );
 };
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,81 @@
+import { useState, useContext } from 'react';
+import { AuthContext } from '../Auth/AuthContext';
+import { postTodo } from '../../api/todos';
+import { Error } from '../../types/Error';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  setErrorsArgument: (argument: Error | null) => void,
+  setTodosList: (arg: Todo[] | []) => void,
+  todos: Todo[] | null,
+};
+
+export const Header: React.FC<Props> = ({
+  setErrorsArgument,
+  setTodosList,
+  todos,
+}) => {
+  const [query, setQuery] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+  const user = useContext(AuthContext);
+
+  const postTodosData = async () => {
+    if (user && query) {
+      setIsAdding(true);
+      const data = {
+        title: query,
+        userId: user.id,
+        completed: false,
+      };
+
+      await postTodo(data)
+        .then(todo => {
+          if (todos) {
+            setTodosList([...todos, todo]);
+          } else {
+            setTodosList([todo]);
+          }
+        })
+        .catch(() => {
+          setErrorsArgument(Error.Add);
+        })
+        .finally(() => {
+          setIsAdding(false);
+          setQuery('');
+        });
+    }
+
+    if (!query) {
+      setErrorsArgument(Error.Empty);
+    }
+  };
+
+  return (
+    <header className="todoapp__header">
+      <button
+        aria-label="active"
+        data-cy="ToggleAllButton"
+        type="button"
+        className="todoapp__toggle-all active"
+      />
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        postTodosData();
+      }}
+      >
+        <input
+          disabled={isAdding}
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          onChange={(event) => {
+            setQuery(event.target.value);
+          }}
+          value={query}
+        />
+      </form>
+    </header>
+  );
+};
