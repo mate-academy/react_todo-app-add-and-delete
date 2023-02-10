5acc660781bf2ecba427bc3e371a8cacada6edbb
Mohaddeseh Mahfoozi <mahfoozimohaddeseh@gmail.com>
2023-02-10 17:41:08 +0330

    Adding, deleting, updating

---
+++ src/components/FormMain.tsx
@@ -0,0 +1,65 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todos: Todo[];
+  onRemove: (todoId: number) => void;
+  onTodoUpdate: (todo: Todo) => void;
+  processedTodos: number[];
+};
+
+export const FormMain: React.FC<Props> = (
+  {
+    todos, onRemove, onTodoUpdate, processedTodos,
+  },
+) => {
+  return (
+    <section className="todoapp__main">
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          className={classNames(
+            'todo',
+            { completed: todo.completed },
+          )}
+        >
+          {processedTodos.includes(todo.id) && (
+            <div className="modal overlay">
+              <div className="modal-background has-background-white-ter" />
+              <div className="loader" />
+            </div>
+          )}
+          <label className="todo__status-label">
+            <input
+              type="checkbox"
+              className="todo__status"
+              checked={todo.completed}
+              onChange={() => onTodoUpdate({
+                ...todo,
+                completed: !todo.completed,
+              })}
+            />
+          </label>
+
+          <span className="todo__title">{todo.title}</span>
+
+          {/* Remove button appears only on hover */}
+          <button
+            type="button"
+            className="todo__remove"
+            onClick={() => onRemove(todo.id)}
+          >
+            ×
+          </button>
+
+          {/* overlay will cover the todo while it is being updated */}
+          {/* <div className="modal overlay">
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
+          </div> */}
+        </div>
+      ))}
+    </section>
+  );
+};
