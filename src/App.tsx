33e886c43a0ab950a731ba0fe0973300bb770e09
Olha Yevstifieieva <hob.bilbo@gmail.com>
2023-02-19 14:41:43 +0200

    Add react app add and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,135 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
-const USER_ID = 0;
+import { getTodos, deleteTodos } from './api/todos';
+import { Header } from './Components/Header';
+import { TodoList } from './Components/TodoList';
+import { Filter } from './Components/Filter';
+import { Error } from './Components/Error';
+import { Todo, TodoStatus } from './types/Todo';
+import { ErrorMessage } from './types/ErrorMessage';
+import { filterTodos } from './Components/filterTodo';
+import { UserIdContext } from './utils/context';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedFilter, setSelectedFilter] = useState(TodoStatus.All);
+  const [errorType, setErrorType] = useState(ErrorMessage.None);
+  const [isErrorShown, setIsErrorShown] = useState(false);
+  const [creatingTodoTitle, setCreatingTodoTitle] = useState('');
+  const [isClearCompleted, setIsClearCompleted] = useState(false);
+
+  const userId = useContext(UserIdContext);
+
+  useEffect(() => {
+    getTodos(userId)
+      .then((userTodos) => setTodos(userTodos))
+      .catch(() => {
+        setErrorType(ErrorMessage.Download);
+        setIsErrorShown(true);
+      });
+  }, []);
+
+  const counterActiveTodos = useMemo(
+    () => todos.reduce((num, todo) => {
+      return todo.completed ? num : num + 1;
+    }, 0),
+    [todos],
+  );
+
+  const counterCompletedTodos = todos.length - counterActiveTodos;
+
+  const filteredTodos = useMemo(
+    () => filterTodos(todos, selectedFilter),
+    [selectedFilter, todos],
+  );
+
+  const showError = useCallback((error: ErrorMessage) => {
+    setErrorType(error);
+    setIsErrorShown(true);
+  }, []);
+  const hideError = useCallback(() => {
+    setIsErrorShown(false);
+  }, []);
+
+  const AddTodo = useCallback(
+    (newTodo: Todo): void => setTodos((oldTodos) => [...oldTodos, newTodo]),
+    [],
+  );
+
+  const DeleteTodo = useCallback(
+    (todoId: number): void => (
+      setTodos((oldTodos) => oldTodos.filter((todo) => todo.id !== todoId))
+    ),
+    [],
+  );
+
+  const onClearCompleted = useCallback(() => {
+    const completedTodos = filterTodos(todos, TodoStatus.Completed);
+
+    setIsClearCompleted(true);
+    hideError();
+
+    Promise.all(
+      completedTodos.map((todo) => deleteTodos(todo.id).then(() => todo.id)),
+    )
+      .then((ids) => {
+        setTodos((oldTodos) => {
+          return oldTodos.filter((todo) => !ids.includes(todo.id));
+        });
+      })
+      .catch(() => {
+        showError(ErrorMessage.Delete);
+      })
+      .finally(() => {
+        setIsClearCompleted(false);
+      });
+  }, [todos]);
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          counterActiveTodos={counterActiveTodos}
+          showError={showError}
+          hideError={hideError}
+          showCreatingTodo={setCreatingTodoTitle}
+          addNewTodo={AddTodo}
+        />
+
+        <TodoList
+          todos={filteredTodos}
+          creatingTodoTitle={creatingTodoTitle}
+          showError={showError}
+          hideError={hideError}
+          DeleteTodo={DeleteTodo}
+          isClearCompleted={isClearCompleted}
+        />
+
+        {todos.length > 0 && (
+          <Filter
+            counterActiveTodos={counterActiveTodos}
+            counterCompletedTodos={counterCompletedTodos}
+            selectedFilter={selectedFilter}
+            onFilterSelect={setSelectedFilter}
+            onClearCompleted={onClearCompleted}
+          />
+        )}
+
+        <Error
+          errorMessage={errorType}
+          isErrorShown={isErrorShown}
+          onErrorClose={() => setIsErrorShown(false)}
+        />
+      </div>
+    </div>
   );
 };
---
+++ src/Components/TodoList/TodoList.tsx
@@ -0,0 +1,74 @@
+import React, { useContext } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { TodoItem } from '../Todo';
+import { Todo } from '../../types/Todo';
+import { UserIdContext } from '../../utils/context';
+import { VariantOfTodo } from '../../types/VariantOfTodo';
+import { ErrorMessage } from '../../types/ErrorMessage';
+
+type Props = {
+  todos: Todo[];
+  creatingTodoTitle: string;
+  isClearCompleted: boolean;
+  showError: (errorType: ErrorMessage) => void;
+  hideError: () => void;
+  DeleteTodo: (todoId: number) => void;
+};
+
+export const TodoList: React.FC<Props> = React.memo(({
+  todos,
+  creatingTodoTitle,
+  isClearCompleted,
+  DeleteTodo,
+  showError,
+  hideError,
+}) => {
+  const userId = useContext(UserIdContext);
+
+  const creatingTodo: VariantOfTodo = !creatingTodoTitle
+    ? null
+    : {
+      id: 0,
+      userId,
+      title: creatingTodoTitle,
+      completed: false,
+    };
+
+  return (
+    <section className="todoapp__main">
+      <TransitionGroup>
+        {todos.map((todo) => (
+          <CSSTransition
+            key={todo.id}
+            timeout={300}
+            classNames="item"
+          >
+            <TodoItem
+              todo={todo}
+              showError={showError}
+              hideError={hideError}
+              DeleteTodo={DeleteTodo}
+              isLoading={todo.completed && isClearCompleted}
+            />
+          </CSSTransition>
+        ))}
+
+        {creatingTodo && (
+          <CSSTransition
+            key={0}
+            timeout={300}
+            classNames="temp-item"
+          >
+            <TodoItem
+              todo={creatingTodo}
+              showError={() => {}}
+              hideError={() => {}}
+              DeleteTodo={() => {}}
+              isLoading
+            />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+    </section>
+  );
+});
