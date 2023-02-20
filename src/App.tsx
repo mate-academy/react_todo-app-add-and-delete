51697915a5935e0427d3cc6cfd1b7abf3b2a94f1
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-02-20 19:52:47 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,142 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import React, { useState, useEffect, useMemo } from 'react';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Todo } from './types/Todo';
+import {
+  getTodos, addTodo, removeTodo, updateTodo,
+} from './api/todos';
+import { FilterType } from './types/FilterType';
+import { getFilteredTodos } from './utils/getFilteredTodos';
+import { ErrorMessage } from './components/ErrorMessage';
+import { ErrorMessages } from './types/ErrorMessages';
 
-const USER_ID = 0;
+export const App: React.FC<Todo[]> = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filterType, setFilterType] = useState(FilterType.All);
+  const [errorType, setErrorType] = useState<ErrorMessages>(ErrorMessages.None);
+  const [isErrorHidden, setIsErrorHidden] = useState(true);
 
-export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const loadTodos = async () => {
+    try {
+      const loadedTodos = await getTodos();
+
+      setTodos(loadedTodos);
+    } catch (error) {
+      setErrorType(ErrorMessages.Load);
+      setIsErrorHidden(false);
+    }
+  };
+
+  const filteredTodos = useMemo(() => {
+    return getFilteredTodos(todos, filterType);
+  }, [filterType, todos]);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  useEffect(() => {
+    const timerId = setTimeout(setIsErrorHidden, 3000, true);
+
+    return () => clearTimeout(timerId);
+  }, [isErrorHidden]);
+
+  const addTodoOnServer = async (todo: Todo) => {
+    try {
+      await addTodo(todo);
+      await loadTodos();
+    } catch (error) {
+      setErrorType(ErrorMessages.Add);
+      setIsErrorHidden(false);
+    }
+  };
+
+  const removeTodoFromServer = async (id: number) => {
+    try {
+      await removeTodo(id);
+      await loadTodos();
+    } catch (error) {
+      setErrorType(ErrorMessages.Delete);
+      setIsErrorHidden(false);
+    }
+  };
+
+  const removeCompletedTodos = () => {
+    const completedTodosId = todos
+      .filter(todo => todo.completed)
+      .map(todo => todo.id);
+
+    completedTodosId.forEach(id => {
+      removeTodoFromServer(id);
+    });
+  };
+
+  const updateTodoOnServer = async (todo: Todo) => {
+    try {
+      await updateTodo(todo);
+      await loadTodos();
+    } catch (error) {
+      setErrorType(ErrorMessages.Update);
+      setIsErrorHidden(false);
+    }
+  };
+
+  const selectAllTodos = (isEverythingDone: boolean) => {
+    todos
+      .filter((todo) => todo.completed !== isEverythingDone)
+      .forEach((todo) => {
+        updateTodoOnServer({
+          ...todo,
+          completed: isEverythingDone,
+        });
+      });
+  };
+
+  const toggleAllTodos = async (isEverythingDone: boolean) => {
+    selectAllTodos(isEverythingDone);
+  };
+
+  const handleErrors = (error: ErrorMessages) => {
+    setErrorType(error);
+    setIsErrorHidden(false);
+  };
+
+  const closeErrorMessage = () => {
+    setIsErrorHidden(true);
+  };
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          addTodoOnServer={addTodoOnServer}
+          todos={todos}
+          toggleAllTodos={toggleAllTodos}
+          handleErrors={handleErrors}
+        />
+        <TodoList
+          todos={filteredTodos}
+          removeTodoFromServer={removeTodoFromServer}
+          updateTodoOnServer={updateTodoOnServer}
+        />
+        <Footer
+          filterBy={setFilterType}
+          filterType={filterType}
+          todos={todos}
+          removeCompletedTodos={removeCompletedTodos}
+        />
+      </div>
+      {errorType && (
+        <ErrorMessage
+          errorType={errorType}
+          isErrorHidden={isErrorHidden}
+          closeErrorMessage={closeErrorMessage}
+        />
+      )}
+    </div>
   );
 };
