8bd5c659cfc3d4a1dfc1cba7a8b6b32030e4dd90
Janis Zurilo <zurilojohn@gmail.com>
2023-03-08 22:50:09 +0200

    +

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -2,7 +2,7 @@ import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
 const USER_ID = 6502;
-const API_URL = `https://mate.academy/students-api`;
+const API_URL = 'https://mate.academy/students-api';
 
 export function wait(delay = 1000) {
   return new Promise((resolve) => setTimeout(resolve, delay));
@@ -15,25 +15,21 @@ export const getTodos = (userId: number) => {
 // Creates new todo method
 export function createTodo(newTodo: Omit<Todo, 'id'>): Promise<Todo> {
   return wait(500)
-    .then(() =>
-      fetch(`${API_URL}/todos?userId=${USER_ID}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newTodo),
-      })
-    )
+    .then(() => fetch(`${API_URL}/todos?userId=${USER_ID}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTodo),
+    }))
     .then((res) => res.json());
 }
 
 // Delete todo method
 export function deleteTodos(todoId: number): Promise<void> {
   return wait(500)
-    .then(() =>
-      fetch(`${API_URL}/todos/${todoId}`, {
-        method: 'DELETE',
-      })
-    )
+    .then(() => fetch(`${API_URL}/todos/${todoId}`, {
+      method: 'DELETE',
+    }))
     .then((res) => res.json());
 }
