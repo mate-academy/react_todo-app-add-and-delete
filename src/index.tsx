4465ef206bf90c25b6173b30eed37ebf45772235
Roshan Jha <kartohajha@gmail.com>
2023-03-29 18:30:14 +0300

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom/client';
+import React, { createRoot } from 'react-dom/client';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
