170c323d651882cfa2e7477a75b90b34370fe6ff
Vitalii Skomskiy <skomskiy@ukr.net>
2023-03-30 14:00:26 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,153 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import classNames from 'classnames';
+import {
+  patchTodo, deleteTodo, getTodos, postTodo,
+} from './api/todos';
+import { Error } from './components/Error/Error';
+import { TypeFilter, Filter } from './components/Filter/Filter';
+import { Item } from './components/Todo/Todo';
+import { Todo } from './types/Todo';
 import { UserWarning } from './UserWarning';
 
-const USER_ID = 0;
+const USER_ID = 123111;
 
 export const App: React.FC = () => {
+  const [listTodo, setListTodo] = useState<Todo[]>([]);
+  const [error, setError] = useState('');
+  const [titleTodo, setTitleTodo] = useState('');
+  const [filterTodo, setFilterTodo] = useState(TypeFilter.ALL);
+
+  const getListTodo = useCallback(async (filter = TypeFilter.ALL) => {
+    try {
+      if (filter !== undefined) {
+        const result = await getTodos(USER_ID, filter);
+
+        setFilterTodo(filter);
+        setListTodo(result);
+      }
+    } catch (e) {
+      setError('Something were wrong, please try again later');
+    }
+  }, []);
+
+  useEffect(() => {
+    getListTodo();
+  }, []);
+
+  const clearError = useCallback(() => setError(''), []);
+
+  const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (titleTodo) {
+      try {
+        await postTodo(
+          { title: titleTodo, userId: USER_ID, completed: false },
+        );
+
+        getListTodo();
+        setTitleTodo('');
+      } catch (e) {
+        setError('Oops, something were wrong, please try again later');
+      }
+    }
+  };
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleTodo(event.target.value);
+  };
+
+  const deleteTodoHandler = (id: number) => {
+    deleteTodo(id).then(() => getListTodo());
+  };
+
+  const changeTodo = (id: number, completed: boolean) => {
+    patchTodo(id, completed).then(() => getListTodo());
+  };
+
+  const clearCompleated = (list: Todo[]) => {
+    return list.filter(obj => obj.completed === true);
+  };
+
+  const clearActive = (list: Todo[]) => {
+    return list.filter(obj => obj.completed === false);
+  };
+
+  const completedTodo = clearCompleated(listTodo);
+  const activeTodo = clearActive(listTodo);
+
+  function deleteAllTodosHandler(todos: Todo[]) {
+    todos.forEach((todo) => deleteTodoHandler(todo.id));
+  }
+
+  function changeAllTodosHandler(todos: Todo[]) {
+    todos.forEach((todo) => changeTodo(todo.id, true));
+  }
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          {activeTodo.length > 0
+            && (
+              <button
+                type="button"
+                className="todoapp__toggle-all active"
+                onClick={() => changeAllTodosHandler(activeTodo)}
+                aria-label="Get all todos done"
+              />
+            )}
+          <form onSubmit={onSubmitHandler}>
+            <input
+              type="text"
+              className="todoapp__new-todo"
+              placeholder="What needs to be done?"
+              value={titleTodo}
+              onChange={onChangeHandler}
+            />
+          </form>
+        </header>
+
+        <section className="todoapp__main">
+          {listTodo.map((todo: Todo) => (
+            <Item
+              todo={todo}
+              key={todo.id}
+              deleteTodo={deleteTodoHandler}
+              changeTodo={changeTodo}
+            />
+          ))}
+        </section>
+
+        {listTodo.length > 0 || filterTodo !== TypeFilter.ALL
+          ? (
+            <footer className="todoapp__footer">
+              <span className="todo-count">
+                {`${listTodo.length} items left`}
+              </span>
+              <Filter setFilter={getListTodo} />
+              <button
+                type="button"
+                className={classNames('todoapp__clear-completed',
+                  {
+                    'todoapp__clear-completed__hidden':
+                      completedTodo.length < 1,
+                  })}
+                onClick={() => deleteAllTodosHandler(completedTodo)}
+              >
+                Clear completed
+              </button>
+            </footer>
+          )
+          : ''}
+      </div>
+
+      {error && <Error errorText={error} errorClear={clearError} />}
+    </div>
   );
 };
