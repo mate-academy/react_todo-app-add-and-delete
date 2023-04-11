b2056195814d256bc79efcad0c982edc12ce4a45
Sasha Dranuta <dranuta97@gmail.com>
2023-04-11 21:36:56 +0300

    Adding and Deleting functionality implemented

---
+++ src/api/todos.ts
@@ -0,0 +1,14 @@
+import { Todo, Newtodo } from '../types/Todo';
+import { client } from '../utils/fetchClient';
+
+export const getTodos = (userId: number) => {
+  return client.get<Todo[]>(`/todos?userId=${userId}`);
+};
+
+export const addTodo = (todo: Newtodo) => {
+  return client.post<Todo>('/todos', todo);
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
