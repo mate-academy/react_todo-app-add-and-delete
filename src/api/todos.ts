bbe1c015cf81fdcd894bf37e58b509b2565e4607
kulakostas <92936150+kulakostas@users.noreply.github.com>
2023-04-12 10:17:11 +0300

    react_todo-app-add-and-delete solution

---
+++ src/api/todos.ts
@@ -0,0 +1,15 @@
+import { NewTodo } from '../types/NewTodo';
+import { Todo } from '../types/Todo';
+import { client } from '../utils/fetchClient';
+
+export const getTodos = (userId: number) => {
+  return client.get<Todo[]>(`/todos?userId=${userId}`);
+};
+
+export const postTodo = (data: NewTodo) => {
+  return client.post<Todo>('/todos', data);
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
