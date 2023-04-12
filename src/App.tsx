7aa59f132ba856e5532590e0d72d07796724d760
vadosik15 <vadim.bratus@gmail.com>
2023-04-12 09:11:37 +0300

    created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,184 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import {
+  FC,
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+} from 'react';
+import { Hearts } from 'react-loader-spinner';
+import {
+  getTodos,
+  addTodo,
+  deleteTodo,
+} from './api/todos';
+
 import { UserWarning } from './UserWarning';
+import { Header } from './components/Header/Header';
+import { Todo } from './types/Todos';
+import { TodoList } from './components/TodoList/TodoList';
+import { Footer } from './components/Footer/Footer';
 
-const USER_ID = 0;
+import { getActiveTodos, getFilteredTodos } from './Helper/Helpers';
+import { FilterType } from './types/FilterType';
+import { ErrorType } from './types/ErrorTypes';
+import { ErrorMessage } from './components/ErrorMassage/ErrorMassage';
 
-export const App: React.FC = () => {
+const USER_ID = 6928;
+
+export const App: FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<ErrorType>(ErrorType.NONE);
+  const [filterType, setFilterType] = useState(FilterType.ALL);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [disableField, setDisableField] = useState(false);
+  const [loadingTodosId, setLoadingTodosId] = useState([0]);
+
+  const showError = (errorName: ErrorType) => {
+    setErrorMessage(errorName);
+    setHasError(true);
+
+    setTimeout(() => {
+      setHasError(false);
+    }, 3000);
+  };
+
+  const handleClosingError = () => {
+    setHasError(false);
+  };
+
+  const visibleTodos = useMemo(() => {
+    return getFilteredTodos(todos, filterType);
+  }, [todos, filterType]);
+
+  const activeTodos = useMemo(() => {
+    return getActiveTodos(visibleTodos);
+  }, [visibleTodos]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      setIsLoading(true);
+
+      try {
+        const todosFromServer = await getTodos(USER_ID);
+
+        setTodos(todosFromServer);
+      } catch {
+        showError(ErrorType.LOAD);
+      }
+
+      setIsLoading(false);
+    };
+
+    loadData();
+  }, []);
+
+  const addNewTodo = async (title: string) => {
+    if (!title.trim()) {
+      showError(ErrorType.TITLE);
+
+      return;
+    }
+
+    const newTodo = {
+      title,
+      userId: USER_ID,
+      completed: false,
+    };
+
+    try {
+      setTempTodo({
+        ...newTodo,
+        id: 0,
+      });
+      setDisableField(true);
+
+      const currNewTodo = await addTodo(USER_ID, newTodo);
+
+      setTodos(prevTodos => [...prevTodos, currNewTodo]);
+    } catch {
+      showError(ErrorType.ADD);
+    }
+
+    setTempTodo(null);
+    setDisableField(false);
+  };
+
+  const removeTodo = async (todoId: number) => {
+    try {
+      setLoadingTodosId(prevIds => [...prevIds, todoId]);
+
+      await deleteTodo(todoId);
+
+      setTodos(prevTodos => (
+        prevTodos.filter(({ id }) => id !== todoId)
+      ));
+    } catch {
+      showError(ErrorType.DELETE);
+    }
+
+    setLoadingTodosId([0]);
+    setHasError(false);
+  };
+
+  const handleDeleteCompleted = useCallback(() => {
+    todos.forEach(({ completed, id }) => {
+      if (completed) {
+        deleteTodo(id);
+      }
+    });
+  }, [visibleTodos]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          onAdd={addNewTodo}
+          disabled={disableField}
+          activeTodos={activeTodos}
+        />
+
+        {isLoading && (
+          <Hearts
+            height="80"
+            width="80"
+            color="#f3e0e0"
+            ariaLabel="hearts-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible
+          />
+        )}
+
+        <TodoList
+          todos={visibleTodos}
+          tempTodo={tempTodo}
+          onDelete={removeTodo}
+          loadingTodosId={loadingTodosId}
+        />
+
+        {todos.length > 0 && (
+          <Footer
+            filterType={filterType}
+            onFilterChange={setFilterType}
+            activeTodos={activeTodos}
+            onDeleteCompleted={handleDeleteCompleted}
+          />
+        )}
+      </div>
+
+      <ErrorMessage
+        errorMessage={errorMessage}
+        hasError={hasError}
+        onClose={handleClosingError}
+      />
+    </div>
   );
 };
