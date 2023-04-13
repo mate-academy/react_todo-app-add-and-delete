5a39be9607fb52fc9a83b011dafaa8b7c694f3ca
Kateryna Savelieva <ksavelyevaaa@gmail.com>
2023-04-13 16:41:38 +0300

    react todo app add and delete

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,97 @@
+import React, {
+  useCallback,
+  useContext,
+  useMemo,
+} from 'react';
+import classNames from 'classnames';
+
+import { deleteTodo } from '../../api/todos';
+import { Todo } from '../../types/Todo';
+import { AppContext } from '../AppContext/AppContext';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    id,
+    title,
+    completed,
+  } = todo;
+
+  const {
+    allTodos,
+    setAllTodos,
+    showError,
+    setShouldShowError,
+    loadingTodosIds,
+    setLoadingTodosIds,
+  } = useContext(AppContext);
+
+  const isLoadingState = useMemo(() => (
+    loadingTodosIds.includes(id)
+  ), [loadingTodosIds]);
+
+  const deleteTodoFromServer = useCallback(async () => {
+    try {
+      setShouldShowError(false);
+      setLoadingTodosIds(prevIds => [...prevIds, id]);
+
+      await deleteTodo(id);
+
+      setAllTodos(prevTodos => prevTodos.filter(prevTodo => (
+        prevTodo.id !== id
+      )));
+    } catch {
+      showError('Unable to delete a todo');
+    } finally {
+      setLoadingTodosIds([0]);
+    }
+  }, [loadingTodosIds, allTodos]);
+  const handleRemoveButtonClick = () => {
+    deleteTodoFromServer();
+  };
+
+  return (
+    <div
+      key={id}
+      className={classNames(
+        'todo',
+        { completed },
+      )}
+    >
+      <label className="todo__status-label">
+        <input
+          type="checkbox"
+          className="todo__status"
+          checked={completed}
+        />
+      </label>
+
+      <span className="todo__title">
+        {title}
+      </span>
+
+      <button
+        aria-label="Remove"
+        type="button"
+        className="todo__remove"
+        onClick={handleRemoveButtonClick}
+      >
+        ×
+      </button>
+
+      <div
+        className={classNames(
+          'modal',
+          'overlay',
+          { 'is-active': isLoadingState },
+        )}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
