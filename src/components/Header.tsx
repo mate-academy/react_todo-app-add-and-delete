b56129aab61541350cf6ae81f5696d2390e70bf1
Maksym Tsarenko <makstsar94@gmail.com>
2023-05-08 12:47:33 +0300

    solution

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,11 +20,9 @@ export const Header: React.FC<Props> = ({
 
   return (
     <header className="todoapp__header">
-      {/* this buttons is active only if there are some active todos */}
 
       <button type="button" className="todoapp__toggle-all active" />
 
-      {/* Add a todo on form submit */}
       <form
         onSubmit={addTodo}
       >
