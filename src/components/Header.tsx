4292e468d854d1d269bf050f3e6ce1e29dbda86e
Maksym Tsarenko <makstsar94@gmail.com>
2023-05-08 11:31:53 +0300

    initial solution

---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { ChangeEvent, FormEvent } from 'react';
+
+type Props = {
+  newTodoTitle: string,
+  setNewTodoTitle: (title: string) => void,
+  addTodo: (event: FormEvent<HTMLFormElement>) => void,
+  isLoading: boolean,
+};
+
+export const Header: React.FC<Props> = ({
+  newTodoTitle,
+  setNewTodoTitle,
+  addTodo,
+  isLoading,
+}) => {
+  const handleAddTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewTodoTitle(event.target.value);
+  };
+
+  return (
+    <header className="todoapp__header">
+      {/* this buttons is active only if there are some active todos */}
+
+      <button type="button" className="todoapp__toggle-all active" />
+
+      {/* Add a todo on form submit */}
+      <form
+        onSubmit={addTodo}
+      >
+        <input
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={newTodoTitle}
+          onChange={handleAddTitle}
+          disabled={isLoading}
+        />
+      </form>
+    </header>
+  );
+};
