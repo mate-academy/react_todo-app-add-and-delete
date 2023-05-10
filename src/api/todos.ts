b1defa6b19c80a9f60a102428fb56a84dc68fc4e
Ihor Chaikovskyi <ihorchaikovskyiweb@gmail.com>
2023-05-10 20:03:48 +0300

    fixed type

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -2,11 +2,16 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
+type PostDataType = {
+  title:string,
+  completed:boolean
+};
+
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-export const postTodo = (userId: number, postData: any) => {
+export const postTodo = (userId: number, postData: PostDataType) => {
   return client.post<Todo>('/todos', {
     'title': postData.title,
     'userId': userId,
