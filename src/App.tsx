b60926d5b38539e345b1867b8b202fc2b4b5b823
Yelisieiev Kim <rbv271@outlook.com>
2023-05-13 08:33:29 +0300

    clear code

--- src/App.tsx
+++ src/App.tsx
@@ -104,9 +104,6 @@ export const App: React.FC = () => {
     getTodosFromServer();
   }, []);
 
-  // eslint-disable-next-line no-console
-  console.log(isProcessingId);
-
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
