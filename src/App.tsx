7cfd95f5d50ecd4e35e91a5d00db417cb9775a2d
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-05-17 20:08:59 +0100

    implemented adding and deleting todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,167 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+} from 'react';
+import classNames from 'classnames';
 import { UserWarning } from './UserWarning';
+import { Todo } from './types/Todo';
+import { getTodos, addTodo, removeTodo } from './api/todos';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { NewTodo } from './components/NewTodo';
+import { Filter } from './types/Filter';
+import { ErrorType } from './types/ErrorType';
+import { TempTodo } from './types/typeDefs';
 
-const USER_ID = 0;
+const USER_ID = 10358;
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [hasError, setHasError] = useState('');
+  const [filterBy, setFilterBy] = useState<Filter>(Filter.All);
+  const [isLoading, setIsLoading] = useState(false);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+
+  const loadTodos = useCallback(async () => {
+    try {
+      const todosFromServer = await getTodos(USER_ID);
+
+      setTodos(todosFromServer);
+    } catch {
+      setHasError(ErrorType.Load);
+      setTimeout(() => setHasError(ErrorType.Load), 3000);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const activeCount = todos.filter(todo => !todo.completed).length;
+  const isCompleted = todos.some(todo => todo.completed);
+
+  const filteredTodos = todos.filter(todo => {
+    switch (filterBy) {
+      case Filter.Completed:
+        return todo.completed;
+
+      case Filter.Active:
+        return !todo.completed;
+
+      case Filter.All:
+      default:
+        return true;
+    }
+  });
+
+  const makeTodo = useCallback(async (title: string) => {
+    setIsLoading(true);
+    setHasError('');
+
+    if (title.trim() === '') {
+      setHasError(ErrorType.Title);
+
+      return;
+    }
+
+    try {
+      const newTodo: TempTodo = {
+        title,
+        userId: USER_ID,
+        completed: false,
+      };
+
+      setTempTodo({ ...newTodo, id: 0 });
+      const addedTodo = await addTodo(newTodo);
+
+      setTodos((prevTodos) => [...prevTodos, addedTodo]);
+    } catch {
+      setHasError(ErrorType.Add);
+    } finally {
+      setTempTodo(null);
+      setIsLoading(false);
+    }
+  }, []);
+
+  const deleteTodo = useCallback(async (todoId: number) => {
+    setHasError('');
+    try {
+      await removeTodo(todoId);
+      setTodos((currentTodos) => (
+        currentTodos.filter(todo => todo.id !== todoId)
+      ));
+    } catch {
+      setHasError(ErrorType.Delete);
+    }
+  }, [todos]);
+
+  const clearCompleted = useCallback(async () => {
+    const todoIds = todos.filter(todo => todo.completed).map(todo => todo.id);
+
+    todoIds.forEach(todoId => deleteTodo(todoId));
+  }, [todos]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          { todos.length ? (
+            <button
+              type="button"
+              className={classNames('todoapp__toggle-all', {
+                hidden: !todos.length,
+                active: !activeCount,
+              })}
+            />
+          ) : null}
+
+          <NewTodo
+            makeTodo={makeTodo}
+            isLoading={isLoading}
+          />
+        </header>
+
+        {todos.length
+          ? (
+            <>
+              <TodoList
+                todos={filteredTodos}
+                tempTodo={tempTodo}
+                deleteTodo={deleteTodo}
+              />
+              <Footer
+                filterBy={filterBy}
+                setFilterBy={setFilterBy}
+                activeCount={activeCount}
+                isCompleted={isCompleted}
+                clearCompleted={clearCompleted}
+              />
+            </>
+          ) : null}
+      </div>
+
+      <div
+        className={classNames(
+          'notification is-danger is-light has-text-weight-normal', {
+            hidden: !hasError,
+          },
+        )}
+      >
+        <button
+          type="button"
+          className="delete"
+          onClick={() => setHasError('')}
+        />
+        {hasError}
+      </div>
+    </div>
   );
 };
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,45 @@
+import React, { memo } from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todo: Todo;
+  deleteTodo: (todoId: number) => void;
+}
+
+export const TodoItem: React.FC<Props> = memo(({
+  todo,
+  deleteTodo,
+}) => {
+  return (
+    <div
+      className={classNames('todo', {
+        completed: todo.completed,
+      })}
+    >
+      <label className="todo__status-label">
+        <input
+          type="checkbox"
+          className="todo__status"
+        />
+      </label>
+
+      <span className="todo__title">{todo.title}</span>
+
+      {/* Remove button appears only on hover */}
+      <button
+        type="button"
+        className="todo__remove"
+        onClick={() => deleteTodo(todo.id)}
+      >
+        ×
+      </button>
+
+      {/* overlay will cover the todo while it is being updated */}
+      <div className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+});
