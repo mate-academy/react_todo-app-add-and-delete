53c0d46a4bc3affd517e20821d6461fd65467934
Zibi95 <sylwester.sob95@gmail.com>
2023-05-26 16:26:44 +0200

    Add deleting Todo functionality

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -5,52 +5,45 @@ import { TodoType } from '../../types/Todo';
 type TodoProps = {
   todo: TodoType;
   loading?: boolean;
+  deleteTodo?: () => void;
 };
 
-export const Todo = ({ todo: { completed, title }, loading }: TodoProps) => {
+export const Todo = ({
+  todo: { completed, title },
+  loading,
+  deleteTodo = () => {},
+}: TodoProps) => {
   const [editing] = useState(false);
 
   return (
-    <div
-      className={
-        classNames('todo', { completed })
-      }
-    >
+    <div className={classNames('todo', { completed })}>
       <label className="todo__status-label">
-        <input
-          type="checkbox"
-          className="todo__status"
-          checked={completed}
-        />
+        <input type="checkbox" className="todo__status" checked={completed} />
       </label>
 
-      {
-        editing
-          ? (
-            <form>
-              <input
-                type="text"
-                className="todo__title-field"
-                placeholder="Empty todo will be deleted"
-                value="Todo is being edited now"
-              />
-            </form>
-          ) : (
-            <>
-              <span
-                className="todo__title"
-              >
-                { title }
-              </span>
-              <button type="button" className="todo__remove">×</button>
-            </>
-          )
-      }
+      {editing ? (
+        <form>
+          <input
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            value="Todo is being edited now"
+          />
+        </form>
+      ) : (
+        <>
+          <span className="todo__title">{title}</span>
+          <button
+            type="button"
+            className="todo__remove"
+            onClick={deleteTodo}
+          >
+            ×
+          </button>
+        </>
+      )}
 
-      <div className={
-        classNames('modal', 'overlay', { 'is-active': loading })
-      }
-      >
+      <div className={classNames('modal', 'overlay', { 'is-active': loading })}>
         <div className="modal-background has-background-white-ter" />
         <div className="loader" />
       </div>
