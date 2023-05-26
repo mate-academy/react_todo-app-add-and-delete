b8ac1cf1dfbd701e3e4c40cc7789b824643e4f86
Zibi95 <sylwester.sob95@gmail.com>
2023-05-26 16:29:18 +0200

    Add form service - adding new todos functionality

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -1,7 +1,29 @@
-export const TodoForm = () => {
+import { FormEventHandler, useRef } from 'react';
+
+type TodoFormProps = {
+  addTodo: (title: string) => void;
+  loading: boolean;
+};
+
+export const TodoForm = ({ addTodo, loading }: TodoFormProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+
+    if (inputRef.current) {
+      const { value } = inputRef.current;
+
+      addTodo(value);
+      inputRef.current.value = '';
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
+        disabled={loading}
+        ref={inputRef}
         type="text"
         className="todoapp__new-todo"
         placeholder="What needs to be done?"
