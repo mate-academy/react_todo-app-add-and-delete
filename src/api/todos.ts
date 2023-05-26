fcf863ed075687f6b801597c52cf4ac0290679db
Zibi95 <sylwester.sob95@gmail.com>
2023-05-26 16:25:45 +0200

    Add fetching functions - post and delete methods

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,20 @@
 import { TodoType } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
+type PostBodyData = {
+  title: string;
+  userId: number;
+  completed: boolean;
+};
+
 export const getTodos = (userId: number) => {
   return client.get<TodoType[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const postTodo = (userId: number, data: PostBodyData) => {
+  return client.post<TodoType>(`/todos?userId=${userId}`, data);
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
