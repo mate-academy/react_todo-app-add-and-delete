1181a88929466f8ad27894b2c902b047a7435a58
Irina Kulish <irinakulish20@gmail.com>
2023-06-02 16:02:08 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,151 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import React, { useEffect, useState, useMemo } from 'react';
+import { Header } from './component/component/Header/header';
+import { TodoList } from './component/component/TodoList/todoList';
+import { Footer } from './component/component/Footer/footer';
+import { getTodos, getTodosPost, deleteTodo } from './api/todos';
+import { Todo } from './type/Todo';
+import { GetFilter } from './type/GetFilter';
+import { NewTodo } from './type/NewTodo';
+import { client } from './utils/fetchClient';
 
-const USER_ID = 0;
+const USER_ID = 10570;
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState(false);
+  const [querySearch, setQuerySearch] = useState('');
+  const [filterBy, setFilterBy] = useState<GetFilter>(GetFilter.ALL);
+
+  const filteredTodos = useMemo(() => {
+    return todos.filter(todo => {
+      switch (filterBy) {
+        case GetFilter.ACTIVE:
+          return !todo.completed;
+        case GetFilter.COMPLETED:
+          return todo.completed;
+        case GetFilter.ALL:
+          return true;
+        default:
+          return todo;
+      }
+    });
+  }, [filterBy, todos]);
+
+  const getTodosServer = async () => {
+    try {
+      const arrayTodos = await getTodos(USER_ID);
+
+      setTodos(arrayTodos);
+      setError(false);
+    } catch {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    getTodosServer();
+  }, []);
+
+  const addTodo = async () => {
+    const newTodo: NewTodo = {
+      userId: USER_ID,
+      completed: false,
+      title: querySearch,
+    };
+
+    try {
+      await getTodosPost(USER_ID, newTodo);
+      await getTodosServer();
+    } catch {
+      setError(true);
+    }
+  };
+
+  const handleDeleteTodo = async (todoId: number) => {
+    try {
+      await deleteTodo(todoId);
+      await getTodosServer();
+    } catch {
+      setError(true);
+    }
+  };
+
+  const deleteTodoCompleted = async () => {
+    todos.filter(todo => todo.completed)
+      .map(todo => handleDeleteTodo(todo.id));
+  };
+
+  const onUpdate = async (id: number) => {
+    const updatedTodo = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodo);
+    try {
+      const todoToUpdate = todos.find((todo) => todo.id === id);
+
+      if (todoToUpdate) {
+        await client.patch(`/todos/${id}`, {
+          completed: !todoToUpdate.completed,
+          title: todoToUpdate.title,
+          userId: USER_ID,
+          id,
+        });
+      }
+    } catch {
+      setError(true);
+    }
+  };
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          querySearch={querySearch}
+          setQuerySearch={setQuerySearch}
+          handleAddTodo={addTodo}
+        />
+
+        <TodoList
+          todos={filteredTodos}
+          deleteTodo={handleDeleteTodo}
+          onUpdate={onUpdate}
+        />
+        {todos.length > 0 && (
+          <Footer
+            todosShow={filteredTodos}
+            filterBy={filterBy}
+            setFilterBy={setFilterBy}
+            deleteTodoCompleted={deleteTodoCompleted}
+          />
+        )}
+      </div>
+
+      {error && (
+        <div className="notification is-danger is-light has-text-weight-normal">
+          <button
+            type="button"
+            className="delete"
+          />
+
+          Unable to add a todo
+          <br />
+          Unable to delete a todo
+          <br />
+          Unable to update a todo
+        </div>
+      )}
+    </div>
   );
 };
