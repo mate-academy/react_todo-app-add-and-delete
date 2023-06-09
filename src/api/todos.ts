33507a03535d5435be6b1d4d9a1b71e31a94565c
catgode <105614630+catgode@users.noreply.github.com>
2023-06-09 03:29:59 +0200

    add a method for sending todo data to the server

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,6 +5,10 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
+export const addTodo = (todo: Omit<Todo, 'id'>) => {
+  return client.post('/todos', todo);
+};
+
 // Add more methods here
 // https://mate.academy/students-api/todos?userId=10624 ctgdprzyb@gmail.com Kacper
 // https://mate.academy/students-api/todos?userId=10625 c47g0d3@gmail.com catgode [LEAVE EMPTY]
