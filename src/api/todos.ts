5059d695736b227840e5dde2091c32821741389f
catgode <105614630+catgode@users.noreply.github.com>
2023-06-09 05:43:35 +0200

    create method for requesting a todo deletion

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -9,6 +9,10 @@ export const addTodo = (todo: Omit<Todo, 'id'>) => {
   return client.post<Todo>('/todos', todo);
 };
 
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
 // Add more methods here
 // https://mate.academy/students-api/todos?userId=10624 ctgdprzyb@gmail.com Kacper
 // https://mate.academy/students-api/todos?userId=10625 c47g0d3@gmail.com catgode [LEAVE EMPTY]
