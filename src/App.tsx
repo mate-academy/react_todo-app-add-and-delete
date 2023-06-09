b499653cf32ab5c2dea0f896fe7ab246db0557a5
catgode <105614630+catgode@users.noreply.github.com>
2023-06-09 03:46:03 +0200

    create an user ID context for use in TodoSubmit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { UserWarning } from './UserWarning';
 import { TodoError, TodoFooter, TodoList } from './components';
 import { getTodos } from './api/todos';
 import { Todo } from './types/Todo';
+import { UserIdContext } from './utils/userIdContext';
 
 const USER_ID = 10624;
 
@@ -23,25 +24,27 @@ export const App: React.FC = () => {
   }
 
   return (
-    <div className="todoapp">
-      <h1 className="todoapp__title">todos</h1>
+    <UserIdContext.Provider value={USER_ID}>
+      <div className="todoapp">
+        <h1 className="todoapp__title">todos</h1>
 
-      <div className="todoapp__content">
-        <TodoList todos={todos} filteringMode={filteringMode} />
-        {/* handle rendering the todo list and the todo entry field */}
+        <div className="todoapp__content">
+          <TodoList todos={todos} filteringMode={filteringMode} />
+          {/* handle rendering the todo list and the todo entry field */}
 
-        {/* Hide the footer if there are no todos */}
-        {todos && (
-          <TodoFooter
-            setFilteringMode={setFilteringMode}
-            filteringMode={filteringMode}
-          />
-        )}
+          {/* Hide the footer if there are no todos */}
+          {todos && (
+            <TodoFooter
+              setFilteringMode={setFilteringMode}
+              filteringMode={filteringMode}
+            />
+          )}
+        </div>
+
+        {/* Notification is shown in case of any error */}
+        {/* Add the 'hidden' class to hide the message smoothly */}
+        {error && <TodoError error={error} setError={setError} />}
       </div>
-
-      {/* Notification is shown in case of any error */}
-      {/* Add the 'hidden' class to hide the message smoothly */}
-      {error && <TodoError error={error} setError={setError} />}
-    </div>
+    </UserIdContext.Provider>
   );
 };
--- src/components/TodoSubmit.tsx
+++ src/components/TodoSubmit.tsx
@@ -1,4 +1,10 @@
+import { useContext } from 'react';
+import { UserIdContext } from '../utils/userIdContext';
+
 export const TodoSubmit: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const userId = useContext(UserIdContext);
+
   return (
     <header className="todoapp__header">
       {/* this buttons is active only if there are some active todos */}
