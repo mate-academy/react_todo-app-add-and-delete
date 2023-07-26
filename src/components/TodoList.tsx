f929589651fdbeacde78b2e5c7d444cd724d9290
yurii-krymskiy <yurii.krymskyi@gmail.com>
2023-07-26 09:12:26 +0200

    sol

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import '../styles/todo.scss';
+import { TodoItem } from './TodoItem';
+
+type Props = {
+  todos: Todo[]
+  handleDelete: (id: number) => void;
+};
+
+export const TodoList: React.FC<Props> = ({ todos, handleDelete }) => {
+  return (
+    <>
+      {todos.map(todo => (
+        <TodoItem
+          todo={todo}
+          handleDelete={handleDelete}
+        />
+
+      ))}
+    </>
+  );
+};
