026fe2b2bc0507d96fa8202fe54f2fba36da82cb
tetianakostiuk <ipz213_kto@student.ztu.edu.ua>
2023-07-31 08:56:25 +0300

    added task solution

---
+++ src/components/TodoInput.tsx
@@ -0,0 +1,30 @@
+import { FC, FormEvent } from 'react';
+
+type Props = {
+  handleFormSubmit: (x: FormEvent<HTMLFormElement>) => void,
+  inputValue: string,
+  setInputValue: (x: string) => void,
+};
+
+export const TodoInput: FC<Props> = ({
+  handleFormSubmit, inputValue, setInputValue,
+}) => {
+  return (
+    <header className="todoapp__header">
+      {/* this buttons is active only if there are some active todos */}
+      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+      <button type="button" className="todoapp__toggle-all active" />
+
+      {/* Add a todo on form submit */}
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+      </form>
+    </header>
+  );
+};
