e84a544fa1405cad0bc11eb53f6c5092ff1b36a8
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-08-04 23:26:22 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,212 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import classNames from 'classnames';
+import { Todo } from './types/Todo';
+import { FILTERS } from './types/FILTERS';
+import * as todoService from './api/todos';
+import { USER_ID } from './utils/constants';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { TodoList } from './components/TodoList';
+import { Filter } from './components/Filter';
+import { NewTodo } from './components/NewTodo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [filter, setFilter] = useState(FILTERS.ALL);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [query, setQuery] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+  const [loading, setLoading] = useState<number[]>([]);
+
+  const addTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setErrorMessage('');
+
+    if (query.trim() === '') {
+      setErrorMessage('Title can\'t be empty');
+      setQuery('');
+
+      return;
+    }
+
+    setIsAdding(true);
+    setTempTodo({
+      id: 0,
+      userId: USER_ID,
+      title: query.trim(),
+      completed: false,
+    });
+    todoService.createTodo({
+      userId: USER_ID,
+      title: query.trim(),
+      completed: false,
+    })
+      .then(newTodo => {
+        setQuery('');
+        setTodos((currentTodos: Todo[]) => [...currentTodos, newTodo]);
+      })
+      .catch(error => {
+        setErrorMessage('Unable to add a todo');
+
+        throw error;
+      })
+      .finally(() => {
+        setIsAdding(false);
+        setTempTodo(null);
+      });
+  };
+
+  const deleteTodo = (todoId: number) => {
+    setErrorMessage('');
+    setLoading(currentId => [...currentId, todoId]);
+
+    todoService.deleteTodo(todoId)
+      .then(() => {
+        setTodos(currentTodos => (
+          currentTodos.filter(currentTodo => currentTodo.id !== todoId)
+        ));
+      })
+      .catch(() => setErrorMessage('Unable to delete a todo'))
+      .finally(() => setLoading([]));
+  };
+
+  const clearCompletedTodos = () => {
+    todos.filter(todo => {
+      if (todo.completed) {
+        deleteTodo(todo.id);
+      }
+
+      return todo;
+    });
+  };
+
+  const visibleTodos = useMemo(() => {
+    switch (filter) {
+      case FILTERS.ALL:
+        return todos;
+
+      case FILTERS.ACTIVE:
+        return todos.filter(todo => !todo.completed);
+
+      case FILTERS.COMPLETED:
+        return todos.filter(todo => todo.completed);
+
+      default:
+        return todos;
+    }
+  }, [todos, filter]);
+
+  useEffect(() => {
+    todoService.getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage('Unable to download todos');
+      });
+  }, []);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | undefined;
+
+    if (errorMessage !== '') {
+      const clearErrorMessage = () => setErrorMessage('');
+
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+
+      timeoutId = setTimeout(clearErrorMessage, 3000);
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [errorMessage]);
+
+  const allTodoComplited = useMemo(() => {
+    return todos.every(todo => todo.completed) && todos.length !== 0;
+  }, [todos]);
+
+  const uncomplitedTodos = useMemo(() => (
+    todos.filter(todo => !todo.completed).length
+  ), [todos]);
+
+  const complitedTodos = useMemo(() => (
+    todos.filter(todo => todo.completed).length
+  ), [todos]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          {todos.length !== 0 && (
+            <button
+              type="button"
+              className={classNames(
+                'todoapp__toggle-all',
+                { active: allTodoComplited },
+              )}
+            />
+          )}
+
+          <NewTodo
+            addTodo={addTodo}
+            query={query}
+            setQuery={setQuery}
+            isAdding={isAdding}
+          />
+        </header>
+
+        <TodoList
+          visibleTodos={visibleTodos}
+          deleteTodo={deleteTodo}
+          loading={loading}
+          tempTodo={tempTodo}
+        />
+
+        {todos.length !== 0 && (
+          <footer className="todoapp__footer">
+            <span className="todo-count">
+              {`${uncomplitedTodos} items left`}
+            </span>
+
+            <Filter
+              filter={filter}
+              setFilter={setFilter}
+            />
+
+            <button
+              type="button"
+              className="todoapp__clear-completed"
+              disabled={complitedTodos === 0}
+              onClick={clearCompletedTodos}
+            >
+              Clear completed
+            </button>
+          </footer>
+        )}
+      </div>
+
+      <div className={classNames(
+        'notification is-danger is-light has-text-weight-normal',
+        { hidden: errorMessage.length === 0 },
+      )}
+      >
+        <button
+          type="button"
+          className="delete"
+          onClick={() => setErrorMessage('')}
+        />
+        {errorMessage}
+      </div>
+    </div>
   );
 };
