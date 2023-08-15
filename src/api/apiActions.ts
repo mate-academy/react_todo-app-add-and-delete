257e868c08628ac97f282cff99a091892756a479
Yaroslav Dudar <dudar.komod@gmail.com>
2023-08-15 04:39:15 +0300

    React TodoAppWithApi

---
+++ src/api/apiActions.ts
@@ -0,0 +1,14 @@
+import { Todo } from '../types/Todo';
+import { client } from '../utils/fetchClient';
+
+export const apiActions = {
+  get: (userId: number) => {
+    return client.get<Todo[]>(`/todos?userId=${userId}`);
+  },
+  delete: (todoId: number) => {
+    return client.delete(`/todos/${todoId}`);
+  },
+  add: (todo: Omit<Todo, 'id'>) => {
+    return client.post<Todo>('/todos', todo);
+  },
+};
