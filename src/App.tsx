e71796062505339a851fb30d427a340c4c09f1eb
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-08-16 16:12:11 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,123 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import React, { useState, useEffect, useMemo } from 'react';
+import classNames from 'classnames';
+import { AddTodo } from './сomponents/AddTodo';
+import { TodosFilter } from './сomponents/TodosFilter';
+import { TodoList } from './сomponents/TodoList';
+import { Todo } from './types/Todo';
+import { getTodos, addTodo, delTodo } from './api/todos';
+import { Filter } from './types/Filter';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>([]);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [filterStatus, setFilterStatus] = useState<Filter>(Filter.all);
+  const [todosIdToDelete, setTodosIdToDelete] = useState<number[]>([]);
+
+  const showError = (message: string) => {
+    setErrorMessage(message);
+    setTimeout(() => setErrorMessage(''), 3000);
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => showError('Unable to load a todos'));
+  }, []);
+
+  useEffect(() => {
+    todosIdToDelete.forEach(id => {
+      delTodo(id)
+        .then(() => {
+          setTodos(curentTodos => curentTodos.filter(todo => todo.id !== id));
+          setTodosIdToDelete(curent => curent.filter(todoId => todoId !== id));
+        })
+        .catch(() => {
+          showError('Unable to delete a todos');
+        });
+    });
+  }, [todosIdToDelete]);
+
+  const createTodo = (newTitle: string) => {
+    setTempTodo({
+      id: 0,
+      userId: 11337,
+      title: newTitle,
+      completed: false,
+    });
+
+    return addTodo(newTitle)
+      .then(newTodo => {
+        setTodos(curentTodos => [...curentTodos, newTodo]);
+      })
+      .catch(() => {
+        showError('Unable to add a todos');
+      })
+      .finally(() => setTempTodo(null));
+  };
+
+  useMemo(() => {
+    switch (filterStatus) {
+      case Filter.active:
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+
+      case Filter.completed:
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+
+      case Filter.all:
+      default:
+        setFilteredTodos([...todos]);
+    }
+  }, [filterStatus, todos]);
+
+  const filteredTodosCount = filteredTodos.length;
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <AddTodo
+          todosLength={filteredTodosCount}
+          createTodo={createTodo}
+          setError={showError}
+        />
+
+        {(!!filteredTodosCount || !!tempTodo) && (
+          <TodoList
+            todos={filteredTodos}
+            tempTodo={tempTodo}
+            todosIdToDelete={todosIdToDelete}
+            setTodosIdToDelete={setTodosIdToDelete}
+          />
+        )}
+
+        {!!todos.length && (
+          <TodosFilter
+            onChangeStatus={setFilterStatus}
+            status={filterStatus}
+            todos={todos}
+            setTodosIdToDelete={setTodosIdToDelete}
+          />
+        )}
+      </div>
+
+      <div className={classNames(
+        'notification is-danger is-light has-text-weight-normal',
+        { hidden: !errorMessage },
+      )}
+      >
+        <button
+          type="button"
+          aria-label="Close error message"
+          className="delete"
+          onClick={() => setErrorMessage('')}
+        />
+        {errorMessage}
+      </div>
+    </div>
   );
 };
---
+++ src/сomponents/TodoItem.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todo: Todo;
+  todosIdToDelete: number[];
+  setTodosIdToDelete: (todosId: number[]) => void;
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  todosIdToDelete,
+  setTodosIdToDelete,
+}) => {
+  const [markLoading, setMarkLoading] = useState(false);
+
+  useEffect(() => {
+    if (todosIdToDelete.includes(todo.id)) {
+      setMarkLoading(true);
+    } else {
+      setMarkLoading(false);
+    }
+  }, [todosIdToDelete]);
+
+  return (
+    <div className="todo">
+      <label className="todo__status-label">
+        <input
+          type="checkbox"
+          className="todo__status"
+        />
+      </label>
+
+      <span className="todo__title">{todo.title}</span>
+      <button
+        type="button"
+        className="todo__remove"
+        onClick={() => setTodosIdToDelete([...todosIdToDelete, todo.id])}
+      >
+        ×
+      </button>
+
+      <div className={classNames(
+        'modal overlay',
+        { 'is-active': markLoading },
+      )}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="Loader">
+          <div className="Loader__content" />
+        </div>
+      </div>
+    </div>
+  );
+};
