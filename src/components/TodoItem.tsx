fb87e534b851268f61dab6aef310a638ef3bd4cf
Piotr <piotrkrasucki@gmail.com>
2023-09-24 17:18:49 +0200

    sol creating TodoItem component to improve removing items proccess

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import cn from 'classnames';
+import { TContext, useTodoContext } from './TodoContext';
+import { Todo } from '../types/Todo';
+import { deleteTodo, getTodos } from '../api/todos';
+
+type Props = {
+  todo: Todo,
+  // key: number,
+};
+
+export const TodoItem: React.FC<Props> = ({ todo }) => {
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const {
+    // todos,
+    setTodos,
+    handleError,
+    // tempTodos,
+    // idTemp,
+  } = useTodoContext() as TContext;
+
+  const handleDelete = (todoId: number) => {
+    setIsDeleting(true);
+
+    const USER_ID = 11550;
+
+    return deleteTodo(todoId)
+      .then(() => getTodos(USER_ID))
+      .then((res) => {
+        setTodos(res);
+        setIsDeleting(false);
+      })
+      .catch(() => {
+        handleError('Unable to delete a todo');
+      });
+  };
+
+  return (
+    <div data-cy="Todo" className={`${todo?.completed ? 'todo completed' : 'todo'}`} key={todo.id}>
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo?.completed}
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo?.title}
+      </span>
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDelete"
+        onClick={() => handleDelete(todo?.id)}
+      >
+        ×
+      </button>
+
+      <div
+        data-cy="TodoLoader"
+        className={cn('modal overlay',
+          { 'is-active': isDeleting === true })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+
+  );
+};
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import cn from 'classnames';
 import { Todo } from '../types/Todo';
-import { deleteTodo, getTodos } from '../api/todos';
 import { TContext, useTodoContext } from './TodoContext';
+import { TodoItem } from './TodoItem';
 
 type Props = {
   todos: Todo[],
@@ -11,61 +11,17 @@ type Props = {
 export const TodoList: React.FC<Props> = ({ todos }) => {
   const {
     // todos,
-    setTodos,
-    handleError,
+    // setTodos,
+    // handleError,
     tempTodos,
     // idTemp,
   } = useTodoContext() as TContext;
 
-  const USER_ID = 11550;
-
-  const handleDelete = (todoId: number) => {
-    return deleteTodo(todoId)
-      .then(() => getTodos(USER_ID))
-      .then((res) => {
-        setTodos(res);
-      })
-      .catch(() => {
-        handleError('Unable to delete a todo');
-      });
-  };
-
   return (
     <section className="todoapp__main" data-cy="TodoList">
       {todos.map((todo) => {
-        // const isLastItem = index === todos.length;
-
         return (
-          <div data-cy="Todo" className={`${todo?.completed ? 'todo completed' : 'todo'}`} key={todo?.id}>
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-                checked={todo?.completed}
-              />
-            </label>
-
-            <span data-cy="TodoTitle" className="todo__title">
-              {todo?.title}
-            </span>
-            <button
-              type="button"
-              className="todo__remove"
-              data-cy="TodoDelete"
-              onClick={() => handleDelete(todo?.id)}
-            >
-              ×
-            </button>
-
-            <div
-              data-cy="TodoLoader"
-              className="modal overlay"
-            >
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div>
+          <TodoItem todo={todo} key={todo.id} />
         );
       })}
 
@@ -87,7 +43,6 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
             type="button"
             className="todo__remove"
             data-cy="TodoDelete"
-          // onClick={() => handleDelete(tempTodo?.id)}
           >
             ×
           </button>
