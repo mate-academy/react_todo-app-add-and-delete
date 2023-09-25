0293e2f9bedcde7ac7a8bccbdfebc724b767bf25
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-09-25 21:37:19 +0300

    implement animation

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,4 +1,8 @@
 import classNames from 'classnames';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 import { Todo } from '../../types/Todo';
 
 type Props = {
@@ -18,90 +22,111 @@ export const Section: React.FC<Props> = ({
 }) => {
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {visibleTodos.map(todo => (
-        <div
-          data-cy="Todo"
-          className={classNames('todo', {
-            completed: todo.completed,
-            active: !todo.completed,
-          })}
-          key={todo.id}
-        >
-          <label className="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              className="todo__status"
-              checked={todo.completed}
-            />
-          </label>
+      <TransitionGroup>
+        {visibleTodos.map(todo => (
+          <CSSTransition
+            key={todo.id}
+            timeout={300}
+            classNames="item"
+          >
+            <div
+              data-cy="Todo"
+              className={classNames('todo', {
+                completed: todo.completed,
+                active: !todo.completed,
+              })}
+              key={todo.id}
+            >
+              <label className="todo__status-label">
+                <input
+                  data-cy="TodoStatus"
+                  type="checkbox"
+                  className="todo__status"
+                  checked={todo.completed}
+                />
+              </label>
 
-          <span data-cy="TodoTitle" className="todo__title">{todo.title}</span>
-          <button
-            type="button"
-            className="todo__remove"
-            data-cy="TodoDelete"
-            onClick={() => onDelete(todo.id)}
-          >
-            ×
-          </button>
+              <span
+                data-cy="TodoTitle"
+                className="todo__title"
+              >
+                {todo.title}
+              </span>
+              <button
+                type="button"
+                className="todo__remove"
+                data-cy="TodoDelete"
+                onClick={() => onDelete(todo.id)}
+              >
+                ×
+              </button>
 
-          <div
-            data-cy="TodoLoader"
-            className={classNames('modal overlay', {
-              'is-active': (selectedId === todo.id && isLoading)
-                || (todo.completed && isLoading && !tempTodo && !selectedId),
-            })}
+              <div
+                data-cy="TodoLoader"
+                className={classNames('modal overlay', {
+                  'is-active': (selectedId === todo.id && isLoading)
+                    || (
+                      todo.completed && isLoading && !tempTodo && !selectedId
+                    ),
+                })}
+              >
+                <div className="modal-background has-background-white-ter" />
+                <div className="loader" />
+              </div>
+            </div>
+          </CSSTransition>
+        ))}
+        {tempTodo && (
+          <CSSTransition
+            key={0}
+            timeout={300}
+            classNames="temp-item"
           >
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
-          </div>
-        </div>
-      ))}
-      {tempTodo && (
-        <div
-          data-cy="Todo"
-          className={classNames('todo', {
-            completed: tempTodo.completed,
-            active: !tempTodo.completed,
-          })}
-          key={tempTodo.id}
-        >
-          <label className="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              className="todo__status"
-              checked={tempTodo.completed}
-            />
-          </label>
+            <div
+              data-cy="Todo"
+              className={classNames('todo', {
+                completed: tempTodo.completed,
+                active: !tempTodo.completed,
+              })}
+              key={tempTodo.id}
+            >
+              <label className="todo__status-label">
+                <input
+                  data-cy="TodoStatus"
+                  type="checkbox"
+                  className="todo__status"
+                  checked={tempTodo.completed}
+                />
+              </label>
 
-          <span
-            data-cy="TodoTitle"
-            className="todo__title"
-          >
-            {tempTodo.title}
-          </span>
-          <button
-            type="button"
-            className="todo__remove"
-            data-cy="TodoDelete"
-            onClick={() => onDelete(tempTodo.id)}
-          >
-            ×
-          </button>
+              <span
+                data-cy="TodoTitle"
+                className="todo__title"
+              >
+                {tempTodo.title}
+              </span>
+              <button
+                type="button"
+                className="todo__remove"
+                data-cy="TodoDelete"
+                onClick={() => onDelete(tempTodo.id)}
+              >
+                ×
+              </button>
 
-          <div
-            data-cy="TodoLoader"
-            className={classNames('modal overlay', {
-              'is-active': isLoading,
-            })}
-          >
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
-          </div>
-        </div>
-      )}
+              <div
+                data-cy="TodoLoader"
+                className={classNames('modal overlay', {
+                  'is-active': isLoading,
+                })}
+              >
+                <div className="modal-background has-background-white-ter" />
+                <div className="loader" />
+              </div>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </section>
   );
 };
