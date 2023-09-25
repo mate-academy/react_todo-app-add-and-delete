90d7394edf06d2cd8b967a1a6daa85a335c4901b
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-09-25 13:00:11 +0200

    t

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -521,8 +521,6 @@ describe('', () => {
 
           page.submitTitle('Hello world');
           cy.wait('@createRequest2');
-          cy.wait(500);
-          page.flushJSTimers();
           todos.assertCount(7);
           todos.assertNotLoading(6);
           todos.assertNotCompleted(6);
