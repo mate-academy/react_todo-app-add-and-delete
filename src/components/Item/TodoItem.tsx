ce9f88bdc4b661d5d9354b2da846ce7238240101
Emil Owczarek <owczarekemil@gmail.com>
2023-09-26 22:09:33 +0200

    add task solution

---
+++ src/components/Item/TodoItem.tsx
@@ -0,0 +1,41 @@
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+
+type TodoItemProps = {
+  todo: Todo;
+  handleDelete: (todoId: number) => void;
+};
+
+export const TodoItem: React.FC<TodoItemProps> = ({ todo, handleDelete }) => {
+  return (
+    <div data-cy="Todo" className={cn('todo', { completed: todo.completed })}>
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      {/* Remove button appears only on hover */}
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDelete"
+        onClick={() => handleDelete(todo.id)}
+      >
+        ×
+      </button>
+      {/* overlay will cover the todo while it is being updated */}
+      <div data-cy="TodoLoader" className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
