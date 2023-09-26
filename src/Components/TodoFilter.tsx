f167f79f9ea283a2f939ffc5539280ab3c0b393b
Patryk Filipiak <patryk.fy@o2.pl>
2023-09-26 02:39:52 +0200

    Solution

---
+++ src/Components/TodoFilter.tsx
@@ -0,0 +1,50 @@
+import { FilterTypes } from '../types/FilterTypes';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todos: Todo[],
+  clearCompletedTodos: () => void,
+  selectedFilter: string,
+  onSelectedFilter: (val: FilterTypes) => void,
+};
+
+export const TodoFilter: React.FC<Props> = ({
+  todos,
+  clearCompletedTodos,
+  selectedFilter,
+  onSelectedFilter,
+}) => {
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {`${todos.filter(({ completed }) => !completed).length} items left`}
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+
+        {Object.values(FilterTypes).map((filterType:FilterTypes) => (
+          <a
+            key={`filter_${filterType}`}
+            data-cy={`FilterLink${filterType}`}
+            href="#/"
+            className={`filter__link ${selectedFilter === filterType ? 'selected' : ''}`}
+            onClick={() => onSelectedFilter(filterType)}
+          >
+            {filterType}
+          </a>
+        ))}
+      </nav>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        onClick={clearCompletedTodos}
+        disabled={!todos.some(({ completed }) => completed)}
+      >
+        Clear completed
+      </button>
+
+    </footer>
+  );
+};
---
+++ src/types/ErrorMessage.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessage {
+  title = 'Title should not be empty',
+  add = 'Unable to add a todo',
+  delete = 'Unable to delete a todo',
+  load = 'Unable to load todos',
+}
