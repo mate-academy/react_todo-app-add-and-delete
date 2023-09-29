327c7a909a06f146b35e18d7c8d5a6224b8ed2a5
Vitalii <fevitaliis@gmail.com>
2023-09-29 10:06:26 +0300

    add

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,37 @@
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+
+type Props = {
+  todos: Todo[],
+  tempTodo: Todo | null,
+  removeTodo: (id: number) => void,
+  loadingItems: number[],
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  tempTodo,
+  removeTodo,
+  loadingItems,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map((todo:Todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          removeTodo={removeTodo}
+          isLoading={loadingItems.includes(todo.id)}
+        />
+      ))}
+
+      {tempTodo && (
+        <TodoItem
+          todo={tempTodo}
+          removeTodo={removeTodo}
+          isLoading
+        />
+      )}
+    </section>
+  );
+};
