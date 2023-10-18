8604ecf7efa8603e1fd8faea4319a3cb9df2747f
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2023-10-18 12:45:51 +0300

    add task solution

---
+++ src/сomponents/TodoList/TodoList.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+type Props = {
+  todos: Todo[];
+  deleteTodo: (id: number) => void;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  deleteTodo,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <TodoItem
+          onDelete={deleteTodo}
+          todo={todo}
+        />
+      ))}
+    </section>
+  );
+};
