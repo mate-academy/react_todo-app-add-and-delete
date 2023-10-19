68242550915c936524fa817eaf2609d55c202196
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-10-19 11:42:25 +0300

    copy base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,135 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import classNames from 'classnames';
 import { UserWarning } from './UserWarning';
+import { Todo } from './types/Todo';
+import { StatusFilter } from './types/Filter';
+import { getTodos } from './api/todos';
+import { TodoList } from './components/TodoList/TodoList';
+import { TodoFilter } from './components/TodoFilter/TodoFilter';
 
-const USER_ID = 0;
+const USER_ID = 11587;
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [statusFilter, setStatusFilter] = useState(StatusFilter.ALL);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage('Unable to load todos');
+        setTimeout(() => {
+          setErrorMessage('');
+        }, 3000);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const filtredTodos: Todo[] = useMemo(() => {
+    let filtered = todos;
+
+    switch (statusFilter) {
+      case StatusFilter.ACTIVE:
+        filtered = filtered.filter(todo => !todo.completed);
+        break;
+
+      case StatusFilter.COMPLETED:
+        filtered = filtered.filter(todo => todo.completed);
+        break;
+
+      default:
+        break;
+    }
+
+    return filtered;
+  }, [todos, statusFilter]);
+
+  const countActiveTodos = todos.filter(todo => !todo.completed).length;
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          {todos.length && (
+            <button
+              type="button"
+              className="todoapp__toggle-all active"
+              data-cy="ToggleAllButton"
+            />
+          )}
+
+          {/* Add a todo on form submit */}
+          <form>
+            <input
+              data-cy="NewTodoField"
+              type="text"
+              className="todoapp__new-todo"
+              placeholder="What needs to be done?"
+            />
+          </form>
+        </header>
+
+        {!loading && (
+          <>
+            <TodoList
+              todos={filtredTodos}
+            />
+
+            {todos.length > 0 && (
+              <footer className="todoapp__footer" data-cy="Footer">
+                <span className="todo-count" data-cy="TodosCounter">
+                  {`${countActiveTodos} items left`}
+                </span>
+
+                {/* Active filter should have a 'selected' class */}
+
+                <TodoFilter
+                  filter={statusFilter}
+                  setFilter={setStatusFilter}
+                />
+
+                {/* don't show this button if there are no completed todos */}
+                <button
+                  type="button"
+                  className="todoapp__clear-completed"
+                  data-cy="ClearCompletedButton"
+                >
+                  Clear completed
+                </button>
+              </footer>
+            )}
+          </>
+        )}
+      </div>
+
+      {/* Notification is shown in case of any error */}
+      {/* Add the 'hidden' class to hide the message smoothly */}
+      <div
+        data-cy="ErrorNotification"
+        className={classNames(
+          'notification is-danger is-light has-text-weight-normal',
+          { hidden: !errorMessage },
+        )}
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={() => setErrorMessage('')}
+        />
+        {errorMessage}
+      </div>
+    </div>
   );
 };
