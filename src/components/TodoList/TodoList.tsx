8f1ff762a2e64d00d467b898fcf84d47627bc98f
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-10-21 11:32:52 +0300

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
-import { TodoItems } from '../TodoItem/TodoItem';
+import { TodoItem } from '../TodoItem/TodoItem';
 
 type Props = {
   todos: Todo[];
@@ -10,7 +10,7 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
   return (
     <section className="todoapp__main" data-cy="TodoList">
       {todos.map(todo => (
-        <TodoItems todo={todo} />
+        <TodoItem todo={todo} />
       ))}
     </section>
   );
