566090d3553066815760b02ae754ac622b5a8265
Victor Hrygorash <viktor12041991@i.ua>
2023-10-30 15:32:13 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,77 @@ export const App: React.FC = () => {
     return <UserWarning />;
   }
 
+  const filteredTodos: Todo[] = useMemo(() => {
+    let preparedTodos = [...todos];
+
+    if (filter !== Filter.ALL) {
+      preparedTodos = preparedTodos.filter((todo) => {
+        switch (filter) {
+          case Filter.ACTIVE:
+            return !todo.completed;
+
+          case Filter.COMPLETED:
+            return todo.completed;
+
+          default:
+            return true;
+        }
+      });
+    }
+
+    return preparedTodos;
+  }, [todos, filter]);
+
+  const deleteTodo = (todoId: number) => {
+    setTodos((currentTodos) => currentTodos
+      .filter((todo) => todo.id !== todoId));
+
+    todosService.deleteTodo(todoId).catch(() => {
+      setTodos(todos);
+      setError('Unable to delete a todo');
+    });
+  };
+
+  function addTodo() {
+    const trimmedTitle = title.trim();
+
+    if (!trimmedTitle) {
+      setError('Title should not be empty');
+
+      return;
+    }
+
+    const data = {
+      userId: USER_ID,
+      title: trimmedTitle,
+      completed: false,
+    };
+
+    setTempTodo({
+      id: 0,
+      ...data,
+    });
+
+    setStatusResponse(true);
+
+    todosService
+      .createTodo(data)
+      .then((newTodo) => {
+        setTitle('');
+        setTodos((currentTodos) => [...currentTodos, newTodo]);
+      })
+      .catch(() => {
+        setError('Unable to add a todo');
+      })
+      .finally(() => {
+        setTempTodo(null);
+        setStatusResponse(false);
+      });
+  }
+
+  const activeTodos = todos.filter((todo) => !todo.completed).length;
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+
   return (
     <section className="section container">
       <p className="title is-4">
