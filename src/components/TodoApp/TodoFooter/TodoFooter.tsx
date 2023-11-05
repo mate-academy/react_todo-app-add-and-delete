a6272cab9c720155900486887511976237acc1a9
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-11-05 16:43:59 +0200

    add task solution

---
+++ src/components/TodoApp/TodoFooter/TodoFooter.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useContext } from 'react';
+
+import * as todoService from '../../../api/todos';
+
+import { DispatchContext, StateContext, actionCreator } from '../../TodoStore';
+import { FilterTodo } from './FilterTodo';
+import { TodoError } from '../../../types/TodoError';
+
+export const TodoFooter: React.FC = () => {
+  const { initialTodos, selectedFilter } = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
+
+  const TotalUncompletedTodos = initialTodos.filter(todo => !todo.completed);
+  const isCompletedTodos = initialTodos.some(todo => todo.completed);
+
+  const deleteCompletedTodos = useCallback(() => {
+    dispatch(actionCreator.toggleClearing());
+    dispatch(actionCreator.clearError());
+
+    const completedTodos = initialTodos.filter(todo => todo.completed);
+    const deletePromises = completedTodos
+      .map(todo => todoService.deleteTodo(todo.id));
+
+    Promise.all(deletePromises)
+      .then(() => {
+        completedTodos.forEach(todo => {
+          dispatch(actionCreator.updateTodos({
+            delete: todo.id, filter: selectedFilter,
+          }));
+        });
+      })
+      .catch(error => {
+        dispatch(actionCreator.addError(TodoError.ErrorDelete));
+        throw error;
+      })
+      .finally(() => dispatch(actionCreator.toggleClearing()));
+  }, [initialTodos]);
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {`${TotalUncompletedTodos.length} ${TotalUncompletedTodos.length === 1 ? 'items' : 'items'} left`}
+      </span>
+
+      <FilterTodo />
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        disabled={!isCompletedTodos}
+        onClick={deleteCompletedTodos}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
---
+++ src/components/TodoApp/TodoHeader/TodoHeader.tsx
@@ -0,0 +1,93 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+import * as todoService from '../../../api/todos';
+
+import {
+  DispatchContext,
+  StateContext,
+  USER_ID,
+  actionCreator,
+} from '../../TodoStore';
+import { TodoError } from '../../../types/TodoError';
+
+export const TodoHeader: React.FC = () => {
+  const { isSubmitting, selectedFilter } = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
+  const [title, setTitle] = useState('');
+  const focusedInput = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (focusedInput.current) {
+      focusedInput.current.focus();
+    }
+  }, [isSubmitting]);
+
+  const handleSubmit = useCallback((
+    event: React.FormEvent<HTMLFormElement>,
+  ) => {
+    event.preventDefault();
+
+    if (!title.trim()) {
+      dispatch(actionCreator.addError(TodoError.ErrorTodo));
+
+      return;
+    }
+
+    dispatch(actionCreator.addTempTodo({
+      id: 0, title: title.trim(), userId: USER_ID, completed: false,
+    }));
+
+    dispatch(actionCreator.toggleSubmitting());
+    dispatch(actionCreator.clearError());
+    todoService.addTodo({
+      title: title.trim(), userId: USER_ID, completed: false,
+    })
+      .then(newTodo => {
+        dispatch(actionCreator.updateTodos({
+          add: newTodo, filter: selectedFilter,
+        }));
+      })
+      .catch(error => {
+        dispatch(actionCreator.addError(TodoError.ErrorAdd));
+        throw error;
+      })
+      .then(() => setTitle(''))
+      .finally(() => {
+        dispatch(actionCreator.addTempTodo(null));
+        dispatch(actionCreator.toggleSubmitting());
+      });
+  }, [title]);
+
+  return (
+    <header className="todoapp__header">
+      {/* this buttons is active only if there are some active todos */}
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+        data-cy="ToggleAllButton"
+      />
+
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input
+          ref={focusedInput}
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          disabled={isSubmitting}
+        />
+      </form>
+    </header>
+  );
+};
