eec4160be86176b0035aab8537b9dee0399a684b
Illia Drach <illyadrach@gmail.com>
2023-11-13 14:09:28 +0200

    test run

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,99 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import React, { useEffect, useState } from 'react';
+import { Todo } from './types/Todo';
+import { Input } from './components/Input/Input';
+import { TodoList } from './components/TodoList/TodoList';
+import { ErrorField } from './components/ErrorField/ErrorField';
+import { getTodos, createTodo, deleteTodos } from './api/todos';
 
-const USER_ID = 0;
+const USER_ID = 11893;
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState('All');
+  const [error, setError] = useState('');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+
+  const activeChecker = todos.some(todo => !todo.completed);
+
+  const displayError = async (errorMessage: string) => {
+    setError(errorMessage);
+
+    setTimeout(() => setError(''), 3000);
+  };
+
+  const addNewTodo = (title: string): void => {
+    const newTodo: Todo = {
+      id: 0,
+      userId: USER_ID,
+      title: title.trim(),
+      completed: false,
+    };
+
+    setTempTodo(newTodo);
+
+    createTodo(newTodo)
+      .then(todo => {
+        setTodos((prevTodos) => [...prevTodos, todo]);
+        setTempTodo(null);
+      })
+      .catch(() => displayError('Unable to add a todo'));
+  };
+
+  const deleteTodo = (id: number) => {
+    deleteTodos(id)
+      .then(() => setTodos(
+        (currentTodos) => currentTodos.filter(todo => todo.id !== id)
+      ))
+      .catch(() => displayError('Unable to delete a todo'));
+  };
+
+  const deleteCompleted = () => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    completedTodos.forEach(todo => deleteTodo(todo.id));
+  };
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(listOfTodos => {
+        let filteredList = listOfTodos;
+
+        if (filter === 'Active') {
+          filteredList = listOfTodos.filter(todo => !todo.completed);
+        }
+
+        if (filter === 'Completed') {
+          filteredList = listOfTodos.filter(todo => todo.completed);
+        }
+
+        setTodos(filteredList);
+      })
+      .catch(() => displayError('Unable to load todos'));
+  }, [filter]);
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Input
+          activeTodo={activeChecker}
+          addNewTodo={addNewTodo}
+          displayError={displayError}
+        />
+
+        <TodoList
+          todos={todos}
+          filter={filter}
+          changeFilter={setFilter}
+          tempTodo={tempTodo}
+          deleteTodo={deleteTodo}
+          deleteCompleted={deleteCompleted}
+        />
+      </div>
+
+      <ErrorField error={error} removeError={setError} />
+    </div>
   );
 };
