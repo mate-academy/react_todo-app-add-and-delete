d27d0be382ce17baf78a191904afab5f0514b965
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-11-20 20:04:43 +0200

    delete comment

--- src/components/TodoFooter.tsx
+++ src/components/TodoFooter.tsx
@@ -62,8 +62,6 @@ export const TodoFooter: React.FC = () => {
         </a>
       </nav>
 
-      {/* don't show this button if there are no completed todos */}
-
       <button
         type="button"
         className="todoapp__clear-completed"
