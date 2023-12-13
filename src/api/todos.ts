0b3cab10034aac9bd84f73a964c98b34dd173ef9
Vladyslav Korobka <vladseipa@gmail.com>
2023-12-13 14:00:50 +0200

    added data type of addTodo method in todo.ts

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,7 +5,7 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-export const addTodo = (data: any) => {
+export const addTodo = (data: Omit<Todo, 'id'>) => {
   return client.post<Todo>('/todos', data);
 };
 
