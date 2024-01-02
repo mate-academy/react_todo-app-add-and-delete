518200a7fd8e11534cdbfcdf38da4e19e6f1c569
Sergej <sergej161995@gmail.com>
2024-01-02 16:37:25 +0200

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './styles/index.scss';
-
 import { App } from './App';
 
 createRoot(document.getElementById('root') as HTMLDivElement)
-  .render(<App />);
+  .render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
