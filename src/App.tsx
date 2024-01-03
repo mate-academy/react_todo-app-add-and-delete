ddc786b06fa95f5fe41a72a28023baa9f68f5331
Krzysztof Janiewicz <spojrzenie@gmail.com>
2024-01-03 22:11:05 +0100

    Removed example section with todo editing

--- src/App.tsx
+++ src/App.tsx
@@ -253,33 +253,6 @@ export const App: React.FC = () => {
               </div>
             </div>
           )}
-
-          {/* This todo is being edited */}
-          <div data-cy="Todo" className="todo">
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-              />
-            </label>
-
-            {/* This form is shown instead of the title and remove button */}
-            <form>
-              <input
-                data-cy="TodoTitleField"
-                type="text"
-                className="todo__title-field"
-                placeholder="Empty todo will be deleted"
-                value="Todo is being edited now"
-              />
-            </form>
-
-            <div data-cy="TodoLoader" className="modal overlay">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div>
         </section>
 
         {todos.length !== 0 ? (
