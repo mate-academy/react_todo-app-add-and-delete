b0e58f987c1f5477d924e2a8565f208455e49e2f
Bartek Zmuda <bertsu@protonmail.com>
2024-01-03 11:57:16 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,135 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { UserWarning } from './UserWarning';
+import { Form } from './components/Form';
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Status } from './types/Status';
+import { getTodos } from './api/todos';
 
-const USER_ID = 0;
+const USER_ID = 12132;
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filtredByStatus, setFiltredByStatus] = useState<Status>(Status.ALL);
+  const [errorNotification, setErrorNotification] = useState<string | null>(
+    null,
+  );
+  const [errorVisible, setErrorVisible] = useState(false);
+
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+
+  const showErrorNotification = (message: string) => {
+    setErrorNotification(message);
+    setErrorVisible(true);
+
+    setTimeout(() => {
+      setErrorVisible(false);
+      setErrorNotification(null);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then((data) => {
+        setTodos(data);
+      })
+      .catch(() => {
+        showErrorNotification('Unable to load todos');
+      });
+  }, []);
+
+  const addNewTodo = (newTodo: Todo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const togleCheck = (id: number) => {
+    const togledTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+
+      return todo;
+    });
+
+    setTodos(togledTodos);
+  };
+
+  const toDelete = (id: number) => {
+    const afterDeleteTodo = todos.filter((todo) => todo.id !== id);
+
+    setTodos(afterDeleteTodo);
+  };
+
+  const filtredTodo = todos.filter((todo) => {
+    switch (filtredByStatus) {
+      case Status.ACTIVE:
+        return !todo.completed;
+      case Status.COMPLETED:
+        return todo.completed;
+      default:
+        return true;
+    }
+  });
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          <button
+            type="button"
+            className="todoapp__toggle-all active"
+            data-cy="ToggleAllButton"
+          />
+          <Form
+            USER_ID={USER_ID}
+            addNewTodo={addNewTodo}
+            showErrorNotification={showErrorNotification}
+            setTempTodo={setTempTodo}
+          />
+        </header>
+
+        {todos.length > 0 && (
+          <>
+            <TodoList
+              toDelete={toDelete}
+              todos={filtredTodo}
+              togleCheck={togleCheck}
+              showErrorNotification={showErrorNotification}
+              tempTodo={tempTodo}
+            />
+
+            <Footer
+              setTodos={setTodos}
+              todos={todos}
+              setFiltredByStatus={setFiltredByStatus}
+              filtredByStatus={filtredByStatus}
+              showErrorNotification={showErrorNotification}
+            />
+          </>
+        )}
+      </div>
+      <div
+        data-cy="ErrorNotification"
+        className={`notification is-danger is-light has-text-weight-normal ${
+          errorVisible ? '' : 'hidden'
+        }`}
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={() => setErrorVisible(false)}
+        />
+        {errorNotification}
+      </div>
+    </div>
   );
 };
