e67508e98211242e5f0e3165ce0a571962257d7b
boytrend0108 <boytrend@gmail.com>
2024-01-25 15:21:23 +0200

    add animation

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,7 +1,8 @@
 import cn from 'classnames';
 import { useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Todo } from '../../types/Todo';
-import { TodoItem } from '../TodoItem';
+import { TodoItem } from '../TodoField';
 import { StateContext } from '../../State/State';
 
 type Props = {
@@ -13,40 +14,59 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
 
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {todos.map(todo => (
-        <TodoItem todo={todo} key={todo.id} />
-      ))}
-
-      {tempTodo && (
-        <div data-cy="Todo" className="todo is-active">
-          <label className="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              className="todo__status"
-            />
-          </label>
-
-          <span data-cy="TodoTitle" className="todo__title">
-            {tempTodo.title}
-          </span>
-
-          <button type="button" className="todo__remove" data-cy="TodoDelete">
-            ×
-          </button>
-
-          {/* 'is-active' class puts this modal on top of the todo */}
-          <div
-            data-cy="TodoLoader"
-            className={cn('modal overlay', {
-              'is-active': true,
-            })}
+      <TransitionGroup>
+        {todos.map(todo => (
+          <CSSTransition
+            key={todo.id}
+            timeout={300}
+            classNames="item"
           >
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
-          </div>
-        </div>
-      )}
+            <TodoItem todo={todo} key={todo.id} />
+          </CSSTransition>
+        ))}
+
+        {tempTodo && (
+          <CSSTransition
+            key={0}
+            timeout={300}
+            classNames="temp-item"
+          >
+            <div data-cy="Todo" className="todo is-active">
+              <label className="todo__status-label">
+                <input
+                  data-cy="TodoStatus"
+                  type="checkbox"
+                  className="todo__status"
+                />
+              </label>
+
+              <span data-cy="TodoTitle" className="todo__title">
+                {tempTodo.title}
+              </span>
+
+              <button
+                type="button"
+                className="todo__remove"
+                data-cy="TodoDelete"
+              >
+                ×
+              </button>
+
+              {/* 'is-active' class puts this modal on top of the todo */}
+              <div
+                data-cy="TodoLoader"
+                className={cn('modal overlay', {
+                  'is-active': true,
+                })}
+              >
+                <div className="modal-background has-background-white-ter" />
+                <div className="loader" />
+              </div>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+
     </section>
   );
 };
