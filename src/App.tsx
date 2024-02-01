c3693cc2344d76e8aeffdba45887f226f8c25737
Alyona Nabok <alyonana42@gmail.com>
2024-02-01 15:54:21 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,151 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import cn from 'classnames';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { getTodos } from './api/todos';
+import { Todo } from './types/Todo';
+import { Status } from './types/Status';
+import { getFilteredTodos } from './services/getFilteredTodos';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { USER_ID } from './constants/user';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [filter, setFilter] = useState<Status>(Status.All);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [deletingPostsIds, setDeletingPostsIds] = useState<number[]>([]);
+
+  const filteredTodos = getFilteredTodos(filter, todos);
+  const activeTodosCount = todos.reduce((acc, cur) => {
+    if (!cur.completed) {
+      return acc + 1;
+    }
+
+    return acc;
+  }, 0);
+
+  const handleCloseError = () => {
+    setErrorMessage('');
+  };
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage('Unable to load todos');
+      });
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setErrorMessage('');
+      clearInterval(timeout);
+    }, 3000);
+
+    return () => {
+      clearInterval(timeout);
+    };
+  }, [errorMessage]);
+
+  const handleFilter = (status: Status) => {
+    setFilter(status);
+  };
+
+  const addNewTodo = (newTodo: Todo) => {
+    setTodos(prev => [...prev, newTodo]);
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos(prev => (
+      prev.filter(todo => todo.id !== id)
+    ));
+  };
+
+  const handleError = (error: string) => {
+    setErrorMessage(error);
+  };
+
+  const handleSetTempTodo = (todo: Todo | null) => {
+    setTempTodo(todo);
+  };
+
+  const handleDeletingPostsIds = (id: number | null) => {
+    if (id) {
+      setDeletingPostsIds(prev => [...prev, id]);
+    } else {
+      setDeletingPostsIds([]);
+    }
+  };
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          onAddTodo={addNewTodo}
+          onError={handleError}
+          onSetTempTodo={handleSetTempTodo}
+        />
+
+        {(!!todos.length || !!tempTodo) && (
+          <TodoList
+            todos={filteredTodos}
+            tempTodo={tempTodo}
+            onDeleteTodo={deleteTodo}
+            onError={handleError}
+            deletingPostsIds={deletingPostsIds}
+            onAddDeletingPostId={handleDeletingPostsIds}
+          />
+        )}
+
+        {!!todos.length && (
+          <Footer
+            activeTodosCount={activeTodosCount}
+            filter={filter}
+            onFilter={handleFilter}
+            todos={todos}
+            onDeleteTodo={deleteTodo}
+            onError={handleError}
+            onAddDeletingPostId={handleDeletingPostsIds}
+          />
+        )}
+
+      </div>
+
+      {/* Notification is shown in case of any error */}
+      {/* Add the 'hidden' class to hide the message smoothly */}
+      <div
+        data-cy="ErrorNotification"
+        className={cn(
+          'notification is-danger is-light has-text-weight-normal',
+          { hidden: !errorMessage },
+        )}
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={handleCloseError}
+        />
+        {/* show only one message at a time */}
+        {errorMessage}
+        {/* <br />
+        Title should not be empty
+        <br />
+        Unable to add a todo
+        <br />
+        Unable to delete a todo
+        <br />
+        Unable to update a todo */}
+      </div>
+    </div>
   );
 };
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import cn from 'classnames';
+import { Status } from '../../types/Status';
+import { Todo } from '../../types/Todo';
+import { deleteTodos } from '../../api/todos';
+
+interface Props {
+  activeTodosCount: number;
+  filter: Status;
+  onFilter: (status: Status) => void;
+  todos: Todo[];
+  onDeleteTodo: (id: number) => void;
+  onError: (error: string) => void;
+  onAddDeletingPostId: (id: number | null) => void;
+}
+
+export const Footer: React.FC<Props> = ({
+  activeTodosCount,
+  filter,
+  onFilter,
+  todos,
+  onDeleteTodo,
+  onError,
+  onAddDeletingPostId,
+}) => {
+  const todosIds = todos
+    .filter(({ completed }) => completed)
+    .map(({ id }) => id);
+
+  const handleDeleteTodos = () => {
+    todosIds.forEach(id => {
+      onAddDeletingPostId(id);
+      deleteTodos(id)
+        .then(() => onDeleteTodo(id))
+        .catch(() => {
+          onError('Unable to delete a todo');
+        })
+        .finally(() => onAddDeletingPostId(null));
+    });
+  };
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {`${activeTodosCount} items left`}
+      </span>
+
+      {/* Active filter should have a 'selected' class */}
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={cn('filter__link',
+            { selected: filter === Status.All })}
+          data-cy="FilterLinkAll"
+          onClick={() => onFilter(Status.All)}
+        >
+          {Status.All}
+        </a>
+
+        <a
+          href="#/active"
+          className={cn('filter__link',
+            { selected: filter === Status.Active })}
+          data-cy="FilterLinkActive"
+          onClick={() => onFilter(Status.Active)}
+        >
+          {Status.Active}
+        </a>
+
+        <a
+          href="#/completed"
+          className={cn('filter__link',
+            { selected: filter === Status.Completed })}
+          data-cy="FilterLinkCompleted"
+          onClick={() => onFilter(Status.Completed)}
+        >
+          {Status.Completed}
+        </a>
+      </nav>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        onClick={handleDeleteTodos}
+        disabled={!todosIds.length}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
