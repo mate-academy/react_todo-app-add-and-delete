f1abb3db9481078d60d15dc790318216eee697cf
Yevhen <steplerjeka@gmail.com>
2024-02-04 11:02:25 +0200

    todo  without error handler

--- src/TodosContext/TodosContext.tsx
+++ src/TodosContext/TodosContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/no-cycle */
 import React, { useEffect, useMemo, useState } from 'react';
