1878507aa0ab8d4742bd680b5e616f786cec5abd
Rostyslav Tkachuk <pro100kor.moon@gmail.com>
2024-02-05 15:53:05 +0200

    all tests passed

---
+++ src/components/TempTodoItem/TempTodoItem.tsx
@@ -0,0 +1,73 @@
+import classNames from 'classnames';
+import React, { useEffect, useRef, useState } from 'react';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TempTodoItem: React.FC<Props> = ({
+  todo,
+}) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+
+    setTimeout(() => setShowLoader(false), 300);
+  });
+
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames('todo', {
+        completed: todo.completed,
+      })}
+      key={todo.id}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          onChange={() => { }}
+        />
+      </label>
+
+      <span
+        data-cy="TodoTitle"
+        className="todo__title"
+      >
+        {todo.title}
+      </span>
+
+      {/* Remove button appears only on hover */}
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDelete"
+        onClick={() => {
+          setShowLoader(true);
+        }}
+      >
+        ×
+      </button>
+
+      {/* 'is-active' class puts this modal on top of the todo */}
+      <div
+        data-cy="TodoLoader"
+        className={classNames('modal', 'overlay', {
+          'is-active': showLoader,
+        })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
