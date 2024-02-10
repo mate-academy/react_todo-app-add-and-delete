770e5ac18f56bfbf4841192462c6bc444def3513
Yevhenii <polotnyak2004@gmail.com>
2024-02-10 17:05:29 +0200

    solution

---
+++ src/components/TodoApp/TodoApp.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useContext } from 'react';
+import { UserWarning } from '../../UserWarning';
+import { USER_ID } from '../../constants/UserId';
+import { TodoContext } from '../../context/TodoContext';
+import { Footer } from '../Footer';
+import { Header } from '../Header';
+import { TodoList } from '../TodoList';
+import { Error } from '../Error';
+
+export const TodoApp: React.FC = () => {
+  const { todos, tempTodo } = useContext(TodoContext);
+
+  const activeTodosCount = todos.filter(({ completed }) => {
+    return !completed;
+  }).length;
+
+  const completedTodosIds = todos
+    .filter(({ completed }) => completed)
+    .map(({ id }) => id);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  return (
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        {!!(todos.length || tempTodo) && (
+          <TodoList tempTodo={tempTodo} />
+        )}
+
+        {todos.length > 0 && (
+          <Footer
+            activeTodosCount={activeTodosCount}
+            completedTodosIds={completedTodosIds}
+          />
+        )}
+      </div>
+
+      <Error />
+    </div>
+  );
+};
