912b33bec5a0acbc35144c6dbf8581ad92a17dd3
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-02-15 10:16:24 +0100

    fixed footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { USER_ID } from './utils/userId';
 
@@ -7,29 +7,31 @@ import { UserWarning } from './UserWarning';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { TodoList } from './components/TodoList';
-import { TodoProvider } from './components/State/TodoContext';
+import { TodoContext } from './components/State/TodoContext';
 import { ErrorNotification } from './components/ErrorNotification';
 
 export const App: React.FC = () => {
+  const { todos } = useContext(TodoContext);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <TodoProvider>
-      <div className="todoapp">
-        <h1 className="todoapp__title">todos</h1>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-        <div className="todoapp__content">
-          <Header />
-
-          <TodoList />
-
-          <Footer />
-        </div>
-
-        <ErrorNotification />
+      <div className="todoapp__content">
+        <Header />
+        <TodoList />
+        {!!todos.length && (
+          <>
+            <Footer />
+          </>
+        )}
       </div>
-    </TodoProvider>
+
+      <ErrorNotification />
+    </div>
   );
 };
