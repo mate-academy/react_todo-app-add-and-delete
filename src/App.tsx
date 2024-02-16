23bb0c5c9b9ea8db5b8a6e9d9f2b58ae4980b7c4
AnoriFrell <anori.fjord@gmail.com>
2024-02-16 13:32:10 +0200

    react_todo-app-add-and-delete with unknown styles issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,151 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import { UserWarning } from './UserWarning';
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filter';
+import { Error } from './types/Error';
+import { ErrorMessage } from './components/ErrorMessage';
+import { addTodos, deleteTodos, getTodos } from './api/todos';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
 
-const USER_ID = 0;
+const USER_ID = 27;
+
+export const filterTodos = (currentTodos: Todo[], query: Filter) => {
+  return currentTodos.filter(todo => {
+    switch (query) {
+      case Filter.All:
+        return todo;
+      case Filter.Completed:
+        return todo.completed;
+      case Filter.Active:
+        return !todo.completed;
+      default:
+        return todo;
+    }
+  });
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>(Filter.All);
+  const [error, setError] = useState<Error | null>(null);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [deleteTodoId, setDeleteTodoId] = useState<number | null>(null);
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => setError(Error.UnableToLoadAll));
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    return filterTodos(todos, filter);
+  }, [todos, filter, filterTodos]);
+
+  const activeTodosCount = useMemo(() => {
+    return todos.filter(todo => todo.completed !== true).length;
+  }, [todos]);
+
+  const isCompletedTodos = useMemo(() => {
+    return todos
+      .filter(todo => todo.completed === true).length > 0;
+  }, [todos]);
+
+  const addNewTodo = useCallback((title: string) => {
+    if (title.trim() === '') {
+      setError(Error.NoTitle);
+
+      return;
+    }
+
+    const temp = {
+      title,
+      id: 0,
+      completed: false,
+      userId: USER_ID,
+    };
+
+    setTempTodo(temp);
+    setError(null);
+
+    addTodos(temp)
+      .then(res => {
+        setTodos(prev => [
+          ...prev,
+          res,
+        ]);
+      })
+      .catch(() => setError(Error.UnableToAdd))
+      .finally(() => {
+        setTempTodo(null);
+      });
+  }, []);
+
+  const deleteCompletedTodos = useCallback(() => {
+    const completedTodoIds = todos
+      .filter(todo => todo.completed)
+      .map(todo => todo.id);
+
+    Promise.all(completedTodoIds.map(id => deleteTodos(id)))
+      .then(() => {
+        setTodos(prev => prev.filter(
+          todo => !completedTodoIds.includes(todo.id),
+        ));
+      })
+      .catch(() => setError(Error.UnableToDelete))
+      .finally(() => {
+        setDeleteTodoId(null);
+      });
+  }, [todos]);
+
+  const deleteCurrentTodo = useCallback((id: number) => {
+    setDeleteTodoId(id);
+    deleteTodos(id)
+      .then(() => {
+        setTodos(prev => prev.filter(todo => todo.id !== id));
+      })
+      .catch(() => setError(Error.UnableToDelete))
+      .finally(() => {
+        setDeleteTodoId(null);
+      });
+  }, []);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">React Todo App - Load Todos</a>
-      </p>
-
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+      <div className="todoapp__content">
+        <Header
+          addTempTodo={addNewTodo}
+          disabled={!!tempTodo}
+        />
+        <TodoList
+          todos={filteredTodos}
+          tempTodo={tempTodo}
+          deleteTodos={deleteCurrentTodo}
+          deleteTodosId={deleteTodoId}
+        />
+        {!!todos.length && (
+          <Footer
+            filterTodos={setFilter}
+            currentFilter={filter}
+            isCompletedTodos={isCompletedTodos}
+            activeTodosCount={activeTodosCount}
+            deleteCompletedTodos={deleteCompletedTodos}
+          />
+        )}
+      </div>
+      {error
+        && <ErrorMessage error={error} close={() => setError(null)} />}
+    </div>
   );
 };
