ad4b4ef575ed73b3db45e098dcb54cfcab44c168
Olena Shapkun <shapkunkh@gmail.com>
2024-02-22 14:47:24 +0200

    todo app p2

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,6 @@
 import React from 'react';
 import { TodosProvider } from './Store';
 import { TodoApp } from './components/TodoApp/TodoApp';
-// import classNames from 'classnames';
-// import * as postService from './api/todos';
-// import { UserWarning } from './UserWarning';
-// import { Todo } from './types/Todo';
-// import { Status } from './types/FilterStatus';
-// import { getFilteredTodos } from './services/getFilteredTodos';
-// import { wait } from './utils/fetchClient';
-// import { ErrorMessages } from './types/ErrorMessages';
-// import { TodoItem } from './components/TodoItem';
-// import { Todolist } from './components/TodoList';
 
 export const App: React.FC = () => {
   return (
