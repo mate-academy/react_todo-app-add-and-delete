67d2e32813e6ac103a0de2562f612c66c02b8a05
Olexandr Rapin <alexrapin04@gmail.com>
2024-03-07 18:34:02 +0200

    solution

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { TodoContext } from '../../context';
+import { ErrorMessage } from '../../types/ErrorMessages';
+import { USER_ID } from '../../constants/constans';
+import * as TodoClient from '../../api/todos';
+
+export const Header: React.FC = () => {
+  const [value, setValue] = useState('');
+  const { todos, tempTodo, addTodo, handleSetTempTodo, handleSetErrorMessage } =
+    useContext(TodoContext);
+
+  const newTodoInput = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    newTodoInput.current?.focus();
+  }, [todos.length, tempTodo]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleSetErrorMessage(ErrorMessage.None);
+
+    const newTodo = {
+      userId: USER_ID,
+      title: value.trim(),
+      completed: false,
+    };
+
+    if (!value.trim()) {
+      handleSetErrorMessage(ErrorMessage.Title);
+
+      return;
+    }
+
+    newTodoInput.current?.setAttribute('disabled', 'true');
+    handleSetTempTodo({ ...newTodo, id: 0 });
+    TodoClient.addTodo(newTodo)
+      .then(todo => {
+        addTodo(todo);
+        setValue('');
+      })
+      .catch(() => handleSetErrorMessage(ErrorMessage.Add))
+      .finally(() => {
+        newTodoInput.current?.removeAttribute('disabled');
+        handleSetTempTodo(null);
+      });
+  };
+
+  return (
+    <header className="todoapp__header">
+      {todos.length > 0 && (
+        <button
+          type="button"
+          className="todoapp__toggle-all active"
+          data-cy="ToggleAllButton"
+        />
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={newTodoInput}
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={value}
+          onChange={event => setValue(event.target.value)}
+        />
+      </form>
+    </header>
+  );
+};
---
+++ src/types/TodoContextType.ts
@@ -0,0 +1,18 @@
+import { ErrorMessage } from './ErrorMessages';
+import { Status } from './Status';
+import { Todo } from './Todo';
+
+export interface TodoContextType {
+  todos: Todo[];
+  filteredTodos: Todo[];
+  status: Status;
+  tempTodo: Todo | null;
+  errorMessage: ErrorMessage;
+  updatingTodosIds: number[];
+  addTodo: (newTodo: Todo) => void;
+  deleteTodo: (todoDeleteID: number) => void;
+  changeStatus: (newStatus: Status) => void;
+  handleSetErrorMessage: (newError: ErrorMessage) => void;
+  handleSetTempTodo: (newTodo: Todo | null) => void;
+  handleUpdatingTodosIds: (id: number | null) => void;
+}
