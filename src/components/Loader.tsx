a911ee03f424058b49ffa953491faf15242bb583
Ihor Prodan <prodanigor0@gmail.com>
2024-03-21 19:38:12 +0100

    taso4ka todo

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,4 @@
-// eslint-disable jsx-a11y/label-has-associated-control
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useContext } from 'react';
 import '../styles/Loader.scss';
 import classNames from 'classnames';
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-// eslint-disable jsx-a11y/label-has-associated-control
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useContext } from 'react';
 import classNames from 'classnames';
 import { Todo } from '../types/Todo';
