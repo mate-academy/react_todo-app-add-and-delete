7d3fe05cd0b102b16b7f121b49d6c471b2baa9d8
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-03-25 07:00:45 +0200

    feat [react-ts]: implement add/delete todo http methods

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,33 @@
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem';
+
+type Props = {
+  todos: Todo[];
+  onDelete: (todoId: number) => void;
+  processedId: number;
+  tempTodo: Todo | null;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  onDelete,
+  processedId,
+  tempTodo,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          processedId={processedId}
+          onDelete={onDelete}
+        />
+      ))}
+
+      {tempTodo && (
+        <TodoItem key={tempTodo.id} todo={tempTodo} processedId={processedId} />
+      )}
+    </section>
+  );
+};
