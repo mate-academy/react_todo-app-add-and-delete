bb2473664d533340b9ec91052cb8ba6cdcd099f3
Vitalii Vashkevych <NuclearSunStar@gmail.com>
2024-04-02 19:31:06 +0300

    Update (Implemented React Transition Group)

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react';
 import { TodoItem } from './TodoItem';
 import { useTodos } from '../utils/TodoContext';
 import { filterTodos } from '../utils/filterTodos';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 export const TodoList: React.FC = () => {
   const { todos, filter, tempTodo } = useTodos();
@@ -10,10 +12,18 @@ export const TodoList: React.FC = () => {
 
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {visibleTodos.map(todo => (
-        <TodoItem todo={todo} key={todo.id} />
-      ))}
-      {tempTodo && <TodoItem todo={tempTodo} key={tempTodo.id} />}
+      <TransitionGroup>
+        {visibleTodos.map(todo => (
+          <CSSTransition key={todo.id} timeout={300} classNames="item">
+            <TodoItem todo={todo} />
+          </CSSTransition>
+        ))}
+        {tempTodo && (
+          <CSSTransition key={0} timeout={300} classNames="temp-item">
+            <TodoItem todo={tempTodo} />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </section>
   );
 };
