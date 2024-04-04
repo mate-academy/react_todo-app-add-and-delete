49c6024ff28bf961ecd118bcb193093593cec0c0
Viktor Kniahnytskyi <vcutler02@gmail.com>
2024-04-04 19:53:03 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+/* eslint-disable jsx-a11y/label-has-associated-control */
 
-const USER_ID = 0;
+import React from 'react';
+import { USER_ID } from './api/todos';
+import { useTodos } from './utils/TodoContext';
+import { UserWarning } from './UserWarning';
+import { Header } from './components/Header';
+import { Foofer } from './components/Foofer';
+import { TodoList } from './components/TodoList';
+import { ErrorNotification } from './components/ErrorNotification';
 
 export const App: React.FC = () => {
+  const { todos } = useTodos();
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header />
+        {!!todos.length && (
+          <>
+            <TodoList />
+            <Foofer />
+          </>
+        )}
+      </div>
+      <ErrorNotification />
+    </div>
   );
 };
