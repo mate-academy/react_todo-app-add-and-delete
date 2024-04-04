6d9db016061344b4967a796e706ca0432ab53a81
Viktor Kniahnytskyi <123034095+viktor-kniahnytskyi@users.noreply.github.com>
2024-04-04 20:08:41 +0300

    Update src/components/Foofer.tsx
    Co-authored-by: Vadym Voiedilo <108701488+vadiimvooo@users.noreply.github.com>

--- src/components/Foofer.tsx
+++ src/components/Foofer.tsx
@@ -62,7 +62,6 @@ export const Foofer: React.FC = () => {
         </a>
       </nav>
 
-      {/* this button should be disabled if there are no completed todos */}
       <button
         type="button"
         className="todoapp__clear-completed"
