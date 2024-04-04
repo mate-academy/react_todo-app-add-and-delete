75328f39cec3433c09d3807a782a0e1239e20cf1
Viktor Kniahnytskyi <123034095+viktor-kniahnytskyi@users.noreply.github.com>
2024-04-04 20:08:32 +0300

    Update src/App.tsx
    Co-authored-by: Vadym Voiedilo <108701488+vadiimvooo@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export const App: React.FC = () => {
 
       <div className="todoapp__content">
         <Header />
-        {!!todos.length && (
+        {todos.length && (
           <>
             <TodoList />
             <Foofer />
