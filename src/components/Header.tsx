bac373e0f8d22e55a79f2c65dc21990a0160abd2
dmitruz <dmitruz970@gmail.com>
2024-04-05 14:23:54 +0300

    add solution

---
+++ src/components/Header.tsx
@@ -0,0 +1,140 @@
+import cn from 'classnames';
+import { Todo } from '../types/Todo';
+import { FormEvent, useEffect, useMemo, useRef, useState } from 'react';
+import { USER_ID, createTodos } from '../api/todos';
+import { ErrorTypes } from '../types/enums';
+import { handleError } from '../utils/services';
+
+type Props = {
+  todos: Todo[];
+  isFocused: boolean;
+  setIsFocused: (isFocused: boolean) => void;
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setErrorMessage: (errorMessage: ErrorTypes) => void;
+  setLoading: React.Dispatch<React.SetStateAction<number[]>>;
+  setTempTodo: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+export const Header: React.FC<Props> = ({
+  todos,
+  isFocused,
+  setIsFocused,
+  setTodos,
+  setErrorMessage,
+  setLoading,
+  setTempTodo,
+}) => {
+  const [isButtonActive, setIsButtonActive] = useState(true);
+  const [isInputDisabled, setIsInputDisabled] = useState(false);
+  const [title, setTitle] = useState('');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const normalisedTitle = title.trim();
+
+  const isSomeTodoCompleted = useMemo(
+    () => todos.some(todo => !todo.completed),
+    [todos],
+  );
+
+  useEffect(() => {
+    if (inputRef.current && isFocused) {
+      inputRef.current.focus();
+    } else {
+      inputRef.current?.blur();
+    }
+  }, [isFocused]);
+
+  useEffect(() => {
+    if (isSomeTodoCompleted) {
+      setIsButtonActive(false);
+    }
+  }, [todos]);
+
+  const onButtonClick = () => {
+    if (isSomeTodoCompleted) {
+      setTodos(prev => prev.map(todo => ({ ...todo, completed: true })));
+    } else {
+      setTodos(prev => prev.map(todo => ({ ...todo, completed: false })));
+    }
+  };
+
+  const onSubmit = (event?: FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+
+    if (normalisedTitle === '') {
+      handleError(ErrorTypes.titleErr, setErrorMessage);
+    } else {
+      setTempTodo((prevTempTodos: Todo[]) => [
+        ...prevTempTodos,
+        {
+          id:
+            prevTempTodos.length > 0
+              ? Math.max(...prevTempTodos.map(prevTodo => prevTodo.id)) + 1
+              : Math.floor(Math.random() * 100),
+          userId: 404,
+          title: normalisedTitle,
+          completed: false,
+        },
+      ]);
+
+      setIsFocused(false);
+      setIsInputDisabled(true);
+
+      createTodos({ userId: USER_ID, completed: false, title: normalisedTitle })
+        .then(resp => {
+          setTodos((prevTodos: Todo[]) => [
+            ...prevTodos,
+            {
+              id: resp.id,
+              userId: resp.userId,
+              title: resp.title,
+              completed: resp.completed,
+            },
+          ]);
+          setIsFocused(true);
+          setTempTodo(() => []);
+          setLoading(prev =>
+            prev.filter(
+              item => item === Math.max(...todos.map(todo => todo.id)),
+            ),
+          );
+          setIsInputDisabled(false);
+          setTitle('');
+        })
+        .catch(() => {
+          setTempTodo(() => []);
+          setIsInputDisabled(false);
+          setIsFocused(true);
+          handleError(ErrorTypes.addErr, setErrorMessage);
+        });
+    }
+  };
+
+  return (
+    <header className="todoapp__header">
+      <button
+        type="button"
+        className={cn('todoapp__toggle-all', {
+          active: isButtonActive,
+        })}
+        data-cy="ToggleAllButton"
+        onClick={() => onButtonClick()}
+      />
+
+      <form onSubmit={onSubmit}>
+        <input
+          data-cy="NewTodoField"
+          ref={inputRef}
+          type="text"
+          className="todoapp__new-todo"
+          disabled={isInputDisabled}
+          placeholder="What needs to be done?"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          onBlur={() => setIsFocused(false)}
+          onFocus={() => setIsFocused(true)}
+        />
+      </form>
+    </header>
+  );
+};
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,115 @@
+import cn from 'classnames';
+
+import { Todo } from '../types/Todo';
+import { FormEvent, useState } from 'react';
+import { Loader } from './Loader';
+
+type Props = {
+  todo: Todo;
+  setSelectedTodo: (todo: Todo | null) => void;
+  loading: number[];
+  selectedTodo: Todo | null;
+  onDelete: (id: number) => void;
+  onPatch: (todo: Todo, event?: React.FormEvent<HTMLFormElement>) => void;
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  setSelectedTodo,
+  loading,
+  selectedTodo,
+  onDelete,
+  onPatch,
+}) => {
+  const [isDoubleClicked, setIsDoubleClicked] = useState<boolean>(false);
+  const [newTitle, setNewTitle] = useState<string>(todo.title);
+  const isTodoChanged =
+    todo.title !== selectedTodo?.title ||
+    selectedTodo?.completed !== todo.completed;
+
+  const onFormSubmit = (event?: FormEvent<HTMLFormElement>) => {
+    if (selectedTodo && isTodoChanged) {
+      setIsDoubleClicked(false);
+      if (event) {
+        onPatch(selectedTodo, event);
+
+        return;
+      }
+
+      onPatch(selectedTodo);
+    } else {
+      setIsDoubleClicked(false);
+      setSelectedTodo(null);
+    }
+  };
+
+  return (
+    <div data-cy="Todo" className={cn('todo', { completed: todo.completed })}>
+      <label
+        aria-label="todo-status"
+        className="todo__status-label"
+        onClick={() => {
+          setSelectedTodo({
+            ...todo,
+            completed: !todo.completed,
+          });
+        }}
+      >
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          onChange={() => {
+            onFormSubmit();
+          }}
+        />
+      </label>
+
+      {selectedTodo?.id === todo.id && isDoubleClicked ? (
+        <form
+          onSubmit={event => onFormSubmit(event)}
+          onBlur={event => onFormSubmit(event)}
+        >
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            autoFocus
+            value={selectedTodo.title}
+            onChange={event => {
+              setNewTitle(event.target.value);
+              setSelectedTodo({
+                ...selectedTodo,
+                title: event.target.value,
+              });
+            }}
+          />
+        </form>
+      ) : (
+        <span
+          data-cy="TodoTitle"
+          className="todo__title"
+          onDoubleClick={() => {
+            setSelectedTodo(todo);
+            setIsDoubleClicked(true);
+          }}
+        >
+          {newTitle}
+        </span>
+      )}
+      {!isDoubleClicked && (
+        <button
+          type="button"
+          className="todo__remove"
+          data-cy="TodoDelete"
+          onClick={() => onDelete(todo.id)}
+        >
+          ×
+        </button>
+      )}
+      <Loader loading={loading} id={todo.id} />
+    </div>
+  );
+};
