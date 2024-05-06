3115291408354db6469ddd5713ada493cf7bcc96
Maryna Karpova <mari.artarea@gmail.com>
2024-05-06 14:09:21 +0200

    test

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,12 +1,12 @@
-import { Todoo } from '../types/Todo';
+import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
 export const USER_ID = 534;
 
 export const getTodos = () => {
-  return client.get<Todoo[]>(`/todos?userId=${USER_ID}`);
+  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-export const postTodo = (newTodo: Todoo) => {
-  return client.post<Todoo>('/todos', newTodo);
+export const postTodo = (newTodo: Todo) => {
+  return client.post<Todo>('/todos', newTodo);
 };
