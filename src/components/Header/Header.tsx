a8f8d0748437e875a84b7872bae65e2c3dea7568
Prytula Olexandr <liga5872@gmail.com>
2024-05-23 09:15:09 +0300

    Added 2/3 part of todos App

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,45 @@
+import React, { FormEvent } from 'react';
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface HeaderProps {
+  todos: Todo[];
+  handleAddTodo: (event: FormEvent<HTMLFormElement>) => void;
+  titleField: React.RefObject<HTMLInputElement>;
+  title: string;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const Header: React.FC<HeaderProps> = ({
+  todos,
+  handleAddTodo,
+  titleField,
+  title,
+  setTitle,
+}) => {
+  const allCompleted = todos.every(todo => todo.completed);
+
+  return (
+    <header className="todoapp__header">
+      {/* this button should have `active` class only if all todos are completed */}
+      <button
+        type="button"
+        className={cn('todoapp__toggle-all', { active: allCompleted })}
+        data-cy="ToggleAllButton"
+      />
+
+      {/* Add a todo on form submit */}
+      <form onSubmit={handleAddTodo}>
+        <input
+          ref={titleField}
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+      </form>
+    </header>
+  );
+};
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import cn from 'classnames';
+
+interface TodoItemProps {
+  todo: Todo;
+  handleDeleteTodo: (id: number) => void;
+  isLoading: boolean;
+}
+
+export const TodoItem: React.FC<TodoItemProps> = ({
+  todo,
+  handleDeleteTodo,
+  isLoading,
+}) => {
+  return (
+    <div
+      key={todo.id}
+      data-cy="Todo"
+      className={classNames('todo', {
+        completed: todo.completed,
+      })}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          onChange={() => {}}
+        />
+      </label>
+      {true ? (
+        <>
+          <span data-cy="TodoTitle" className="todo__title">
+            {todo.title}
+          </span>
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={() => handleDeleteTodo(todo.id)}
+          >
+            ×
+          </button>
+        </>
+      ) : (
+        <form>
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+          />
+        </form>
+      )}
+
+      <div
+        data-cy="TodoLoader"
+        className={cn('modal overlay', {
+          'is-active': isLoading,
+        })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
