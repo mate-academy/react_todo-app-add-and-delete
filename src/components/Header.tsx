d6eb6ac12817a1fe03f841d7ff60f80fe5f54d28
Vladyslav Shylin <indiggoo228@gmail.com>
2024-05-24 17:19:24 +0300

    add task solution

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useEffect, useRef } from 'react';
+
+type Props = {
+  addTodo: (todoTitle: string) => void;
+  title: string;
+  setTitle: (title: string) => void;
+  isLoading: boolean;
+};
+
+export const Header: React.FC<Props> = ({
+  addTodo,
+  title,
+  setTitle,
+  isLoading,
+}) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (inputRef.current && !isLoading) {
+      inputRef.current.focus();
+    }
+  }, [isLoading]);
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    addTodo(title);
+  };
+
+  return (
+    <header className="todoapp__header">
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+        data-cy="ToggleAllButton"
+      />
+
+      <form onSubmit={onSubmit}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          ref={inputRef}
+          disabled={isLoading}
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+      </form>
+    </header>
+  );
+};
