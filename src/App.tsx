f85e81f3bee3266ec213269caef1f0b7e4923080
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2024-05-29 20:15:17 +0300

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,105 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { USER_ID } from './api/todos';
+import { client } from './utils/fetchClient';
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList/TodoList';
+import { Filter } from './types/Filter';
+import { TodoFooter } from './components/TodoFooter/TodoFooter';
+import { ErrorMessage } from './components/EroorMessage/ErrorMessage';
+import { TodoInput } from './components/TodoInput/TodoInput';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [filter, setFilter] = useState<Filter>(Filter.All);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [todosToDelete, setTodosToDelete] = useState<number[]>([]);
+
+  const getFilteredTodos = () => {
+    switch (filter) {
+      case Filter.Active:
+        return todos.filter(todo => !todo.completed);
+      case Filter.Completed:
+        return todos.filter(todo => todo.completed);
+      default:
+        return todos;
+    }
+  };
+
+  const todosToRender = getFilteredTodos();
+
+  useEffect(() => {
+    client
+      .get<Todo[]>(`/todos?userId=${USER_ID}`)
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage('Unable to load todos');
+      });
+  }, []);
+
+  useEffect(() => {
+    if (errorMessage) {
+      timeoutRef.current = setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+    }
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = null;
+      }
+    };
+  }, [errorMessage]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <TodoInput
+          setErrorMessage={setErrorMessage}
+          setTodos={setTodos}
+          setTempTodo={setTempTodo}
+          inputRef={inputRef}
+        />
+
+        {!!todos.length && (
+          <>
+            <TodoList
+              todos={todosToRender}
+              tempTodo={tempTodo}
+              setTodos={setTodos}
+              setErrorMessage={setErrorMessage}
+              inputRef={inputRef}
+              todosToDelete={todosToDelete}
+              setTodosToDelete={setTodosToDelete}
+            />
+
+            {/* Hide the footer if there are no todos */}
+            <TodoFooter
+              todos={todos}
+              setFilter={setFilter}
+              filter={filter}
+              setTodosToDelete={setTodosToDelete}
+            />
+          </>
+        )}
+      </div>
+
+      <ErrorMessage
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      />
+    </div>
   );
 };
---
+++ src/api/todos.ts
@@ -0,0 +1,18 @@
+import { Todo } from '../types/Todo';
+import { client } from '../utils/fetchClient';
+
+export const USER_ID = 698;
+
+export const getTodos = () => {
+  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+};
+
+export const createTodo = (data: Omit<Todo, 'id'>) => {
+  return client.post<Todo>('/todos', data);
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
+
+// Add more methods here
