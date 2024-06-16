99c9c6cb1bf87a445b68aa8f8ffaaefbb0cfa4ff
ArsenSkandalenko <arsenskandal@gmail.com>
2024-06-16 15:28:07 +0300

    add task solution

---
+++ src/Components/TodoList.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable import/extensions */
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+
+type Props = {
+  todos: Todo[];
+  loadingTodoIds?: number[];
+  handleToggleTodo: (id: number) => void;
+  onDeleteTodo?: (currentTodoId: number) => Promise<void>;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  loadingTodoIds,
+  handleToggleTodo = () => {},
+  onDeleteTodo = () => {},
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onloadingTodoIds={loadingTodoIds}
+          handleToggleTodo={handleToggleTodo}
+          handleTodoDelete={onDeleteTodo}
+        />
+      ))}
+    </section>
+  );
+};
