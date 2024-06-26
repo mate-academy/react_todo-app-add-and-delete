f52b6cf769bedd72280a1ea02210be5dbae70ea8
Oksana Pokhvalenko <oksanapokhvalenko@gmail.com>
2024-06-26 23:01:02 +0200

    add soltion

---
+++ src/enums/errorText.ts
@@ -0,0 +1,7 @@
+export enum ErrorText {
+  Loading = 'Error loading todos',
+  Adding = 'Unable to add a todo',
+  Deletion = 'Error deleting todo',
+  Title = 'Title should not be empty',
+  Update = 'Error updating todo',
+}
