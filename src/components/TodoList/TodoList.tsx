e5e4628be44103e6a9fb34da5cbc426710b1e314
Dmytro <107893428+dima91020@users.noreply.github.com>
2024-07-06 21:32:33 +0300

    add task solution

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem/TodoItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+type Props = {
+  todos: Todo[];
+  tempTodo: Todo | null;
+  isLoading: number[];
+  isSubmitting: boolean;
+  onDelete: (todoId: number) => void;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  tempTodo,
+  isLoading,
+  isSubmitting,
+  onDelete,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      <TransitionGroup>
+        {todos.map(todo => (
+          <CSSTransition key={todo.id} timeout={300} classNames="item">
+            <TodoItem
+              todo={todo}
+              key={todo.id}
+              isLoading={isLoading.includes(todo.id)}
+              isSubmitting={isSubmitting}
+              onDelete={onDelete}
+            />
+          </CSSTransition>
+        ))}
+        {tempTodo && (
+          <CSSTransition timeout={300} classNames="item">
+            <TodoItem
+              todo={tempTodo}
+              isLoading={isLoading.includes(0)}
+              isSubmitting={isSubmitting}
+              onDelete={() => {}}
+            />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+    </section>
+  );
+};
