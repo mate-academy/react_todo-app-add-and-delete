6b6e35e22ea5c6e6bae89f0410ed2d4da6717e74
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2024-07-09 16:48:30 +0300

    add not finished task solution

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+
+type Props = {
+  filteredTodos: Todo[];
+  tempTodo: Todo | null;
+  isDeleteing: boolean;
+  handleDelete: (id: number) => void;
+  todoForDelete: number;
+};
+
+export const TodoList: React.FC<Props> = ({
+  filteredTodos,
+  tempTodo,
+  isDeleteing,
+  handleDelete,
+  todoForDelete,
+}) => (
+  <section className="todoapp__main" data-cy="TodoList">
+    {filteredTodos.map(todo => (
+      <TodoItem
+        todo={todo}
+        key={todo.id}
+        isDeleteing={isDeleteing}
+        handleDelete={handleDelete}
+        todoIdForDelete={todoForDelete}
+      />
+    ))}
+    {tempTodo !== null && (
+      <TodoItem todo={tempTodo} isTempTodo={true} handleDelete={handleDelete} />
+    )}
+  </section>
+);
