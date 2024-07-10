1e24801db495e7c4f2eedd7611d1c5024e83e07b
ogchy <ogchyx@gmail.com>
2024-07-10 15:58:19 +0300

    add task solution

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -4,6 +4,7 @@ import { getFilteredTodos } from '../components/filteredTodos';
 import { Todo } from '../types/Todo';
 import { FilterTypes } from '../types/filterTypes';
 import classNames from 'classnames';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 interface TodoListProps {
   todos: Todo[];
@@ -22,62 +23,77 @@ export const TodoList: React.FC<TodoListProps> = ({
 
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {filteredTodos.map(todo => (
-        <div
-          key={todo.id}
-          data-cy="Todo"
-          className={classNames('todo', { completed: todo.completed })}
-        >
-          <label className="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              className="todo__status"
-              checked={todo.completed}
-            />
-          </label>
-
-          <span data-cy="TodoTitle" className="todo__title">
-            {todo.title}
-          </span>
-
-          {/* Remove button appears only on hover */}
-          <button
-            type="button"
-            className="todo__remove"
-            data-cy="TodoDelete"
-            onClick={() => todo.id !== undefined && deleteTodo(todo.id)}
-          >
-            ×
-          </button>
-        </div>
-      ))}
-      {fakeTodo && (
-        <>
-          <div data-cy="TodoLoader" className="modal overlay">
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
-          </div>
+      <TransitionGroup>
+        {filteredTodos.map(todo => (
           <div
-            key={fakeTodo.id}
+            key={todo.id}
             data-cy="Todo"
-            className={classNames('todo', { completed: fakeTodo.completed })}
+            className={classNames('todo', { completed: todo.completed })}
           >
             <label className="todo__status-label">
               <input
                 data-cy="TodoStatus"
                 type="checkbox"
                 className="todo__status"
-                checked={fakeTodo.completed}
+                checked={todo.completed}
               />
             </label>
 
             <span data-cy="TodoTitle" className="todo__title">
-              {fakeTodo.title}
+              {todo.title}
             </span>
+
+            {/* Remove button appears only on hover */}
+            <button
+              type="button"
+              className="todo__remove"
+              data-cy="TodoDelete"
+              onClick={() => todo.id !== undefined && deleteTodo(todo.id)}
+            >
+              ×
+            </button>
           </div>
-        </>
-      )}
+        ))}
+        {fakeTodo && (
+          <CSSTransition key="fakeTodo" timeout={300} classNames="temp-item">
+            <div>
+              <div data-cy="TodoLoader" className="modal overlay">
+                <div className="modal-background has-background-white-ter" />
+                <div className="loader" />
+              </div>
+              <div
+                data-cy="Todo"
+                className={classNames('todo', {
+                  completed: fakeTodo.completed,
+                })}
+              >
+                <label className="todo__status-label">
+                  <input
+                    data-cy="TodoStatus"
+                    type="checkbox"
+                    className="todo__status"
+                    checked={fakeTodo.completed}
+                  />
+                </label>
+
+                <span data-cy="TodoTitle" className="todo__title">
+                  {fakeTodo.title}
+                </span>
+                <button
+                  type="button"
+                  className="todo__remove"
+                  data-cy="TodoDelete"
+                  onClick={() =>
+                    fakeTodo.id !== undefined && deleteTodo(fakeTodo.id)
+                  }
+                >
+                  ×
+                </button>
+              </div>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </section>
   );
 };
