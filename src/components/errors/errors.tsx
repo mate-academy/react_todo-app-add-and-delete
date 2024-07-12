a0a0fbe8e9fa5b78153286f9526dca1eb315a2c8
priemssonia <priemssonia@gmail.com>
2024-07-12 12:19:24 +0300

    solution

---
+++ src/components/errors/errors.tsx
@@ -0,0 +1,24 @@
+import cn from 'classnames';
+import React from 'react';
+
+type Props = {
+  errorMessage: string;
+  onClose: (message: string) => void;
+};
+
+export const Errors: React.FC<Props> = ({ errorMessage, onClose }) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={`notification is-danger is-light has-text-weight-normal ${!errorMessage && 'hidden'}`}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className={cn('delete', { hidden: !errorMessage })}
+        onClick={() => onClose('')}
+      />
+      {errorMessage}
+    </div>
+  );
+};
