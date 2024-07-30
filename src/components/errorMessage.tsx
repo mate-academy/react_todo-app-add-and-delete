b69032655bbe65f2a94990ba032123ced4effac2
Oksana Palahecha <oksanapalahecha@gmail.com>
2024-07-30 16:13:05 +0300

    solution need help with tests

---
+++ src/components/errorMessage.tsx
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+import React from 'react';
+
+type Props = {
+  error: string | null;
+  setError: (errorMesage: string) => void;
+};
+
+export const ErrorMessage: React.FC<Props> = ({ error, setError }) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification is-danger is-light has-text-weight-normal',
+        {
+          hidden: !error,
+        },
+      )}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setError('')}
+      />
+      {error}
+    </div>
+  );
+};
---
+++ src/components/todoFilter.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Filters } from '../types/Filters';
+import { Todo } from '../types/Todo';
+
+interface Props {
+  currentFilter: Filters;
+  onFilterChange: (filter: Filters) => void;
+  todos: Todo[];
+  activeTodosCount: number;
+  onClearCompleted: () => void;
+}
+
+export const TodoFilter: React.FC<Props> = ({
+  currentFilter,
+  onFilterChange,
+  todos,
+  activeTodosCount,
+  onClearCompleted,
+}) => {
+  const completedTodosCount = todos.length - activeTodosCount;
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {activeTodosCount} items left
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={`filter__link ${currentFilter === Filters.All ? 'selected' : ''}`}
+          data-cy="FilterLinkAll"
+          onClick={() => onFilterChange(Filters.All)}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={`filter__link ${currentFilter === Filters.Active ? 'selected' : ''}`}
+          data-cy="FilterLinkActive"
+          onClick={() => onFilterChange(Filters.Active)}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={`filter__link ${currentFilter === Filters.Completed ? 'selected' : ''}`}
+          data-cy="FilterLinkCompleted"
+          onClick={() => onFilterChange(Filters.Completed)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      {completedTodosCount > 0 && (
+        <button
+          type="button"
+          className="clear-completed"
+          onClick={onClearCompleted}
+          data-cy="ClearCompletedButton"
+        >
+          Clear completed
+        </button>
+      )}
+    </footer>
+  );
+};
