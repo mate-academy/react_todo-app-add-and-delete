6bbc76d7603653d20b13428e364aa50847e8cf43
Oleksandr <alex.volkovskyi@gmail.com>
2024-08-06 13:42:58 +0300

    add task solution

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,67 @@
+import classNames from 'classnames';
+import React, { useMemo } from 'react';
+import { Status } from '../../types/Status';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Todo[];
+  todoStatus: Status;
+  setTodoStatus: (status: Status) => void;
+  onDelete: (todoId: number) => void;
+};
+
+export const Footer: React.FC<Props> = ({
+  todos,
+  todoStatus,
+  setTodoStatus,
+  onDelete,
+}) => {
+  const hasCompleted = useMemo(() => {
+    return todos.some(todo => todo.completed);
+  }, [todos]);
+
+  const activeTodosCount = useMemo(() => {
+    return todos.reduce((sum, todo) => sum + Number(!todo.completed), 0);
+  }, [todos]);
+
+  const deleteCompleted = () => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    completedTodos.forEach(todo => onDelete(todo.id));
+  };
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {activeTodosCount} items left
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        {Object.values(Status).map(status => (
+          <a
+            key={status}
+            href={`#/${status}`}
+            className={classNames('filter__link', {
+              selected: status === todoStatus,
+            })}
+            data-cy={`FilterLink${status}`}
+            onClick={() => setTodoStatus(status)}
+          >
+            {status}
+          </a>
+        ))}
+      </nav>
+
+      {/* this button should be disabled if there are no completed todos */}
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        disabled={!hasCompleted}
+        onClick={deleteCompleted}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,86 @@
+import classNames from 'classnames';
+import React, { useEffect, useMemo, useRef } from 'react';
+import { Todo } from '../../types/Todo';
+import { USER_ID } from '../../api/todos';
+
+type Props = {
+  title: string;
+  setTitle: (title: string) => void;
+  errorMessage: string;
+  setErrorMessage: (message: string) => void;
+  resetError: () => void;
+  addTodo: (todo: Omit<Todo, 'id'>) => Promise<void>;
+  completed: boolean;
+  todos: Todo[];
+  submitting: boolean;
+};
+
+export const Header: React.FC<Props> = ({
+  title,
+  setTitle,
+  errorMessage,
+  setErrorMessage,
+  resetError,
+  addTodo,
+  completed,
+  todos,
+  submitting,
+}) => {
+  const formField = useRef<HTMLInputElement>(null);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setErrorMessage('');
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!title.trim()) {
+      setErrorMessage('Title should not be empty');
+      setTimeout(resetError, 3000);
+
+      return;
+    }
+
+    addTodo({
+      userId: USER_ID,
+      title,
+      completed,
+    });
+  };
+
+  const allCompleted = useMemo(() => {
+    return todos.every(todo => todo.completed);
+  }, [todos]);
+
+  useEffect(() => {
+    formField.current?.focus();
+  }, [todos, errorMessage]);
+
+  return (
+    <header className="todoapp__header">
+      {/* this button should have `active` class only if all todos are completed */}
+      <button
+        type="button"
+        className={classNames('todoapp__toggle-all', {
+          active: allCompleted,
+        })}
+        data-cy="ToggleAllButton"
+      />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={formField}
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={title}
+          onChange={handleTitleChange}
+          disabled={submitting}
+        />
+      </form>
+    </header>
+  );
+};
