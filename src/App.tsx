79e573acd713029b50c4116ad3ccd7c830b99f1b
BoGoBooM <MyPikcha@gmail.com>
2024-08-07 18:32:06 +0300

    files was updated

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   getTodos,
   updateTodo,
 } from './api/todos';
+// eslint-disable-next-line import/extensions
 import { ErrorNotification } from './components/ErrorNotification.tsx';
 import { Footer } from './components/Footer.tsx';
 import { Header } from './components/Header.tsx';
