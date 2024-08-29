dc8287f073b19e8e3be2fad89ee0297549efca89
Anuta Kuzmych <a.kusmych@gmail.com>
2024-08-29 22:34:34 +0300

    solution

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames';
+
+import { Todo } from '../../types/Todo';
+import { Loader } from '../Loader/Loader';
+
+type TodoItemProps = {
+  todo: Todo;
+  loading: number[];
+  handleDeleteTodo: (todoId: number) => void;
+};
+
+export const TodoItem: React.FC<TodoItemProps> = ({
+  todo,
+  loading,
+  handleDeleteTodo,
+}) => {
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames('todo', { completed: todo.completed })}
+    >
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDelete"
+        onClick={() => handleDeleteTodo(todo.id)}
+      >
+        ×
+      </button>
+
+      <Loader todo={todo} loading={loading} />
+    </div>
+  );
+};
