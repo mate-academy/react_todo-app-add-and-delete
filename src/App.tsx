4ff932110c19773815f05bbdc877f60db1c5a6cc
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2024-09-21 10:44:53 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,142 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { USER_ID } from './api/todos';
+import * as todoService from './api/todos';
+import { Todo } from './types/Todo';
+import { Header } from './components/Header/Header';
+import { TodoList } from './components/TodoList/TodoList';
+import { Footer } from './components/Footer/Footer';
+import { ErrorMessage } from './components/ErrorMessage/ErrorMessage';
+import { TodoStatusFilter } from './types/TodoStatusFilter';
+import { getPreparedTodos } from './utils/getPreparedTodos';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [processingTodosId, setProcessingTodosId] = useState<number[]>([]);
+  const [selectedFilter, setSelectedFilter] = useState<TodoStatusFilter>(
+    TodoStatusFilter.All,
+  );
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+
+  const applyErrorForSomeTime = (errorText: string, delayToRemove = 3000) => {
+    setErrorMessage(errorText);
+
+    setTimeout(() => setErrorMessage(''), delayToRemove);
+  };
+
+  const activeTodosCount = todos.filter(todo => !todo.completed).length;
+  const completedTodosCount = todos.filter(todo => todo.completed).length;
+  const preparedTodos = getPreparedTodos(todos, selectedFilter);
+
+  useEffect(() => {
+    todoService
+      .getTodos()
+      .then(setTodos)
+      .catch(() => applyErrorForSomeTime('Unable to load todos'));
+  }, []);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
-  return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+  const addTodo = async ({ id, title, userId, completed }: Todo) => {
+    try {
+      if (title.length === 0) {
+        throw new Error();
+      }
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      setTempTodo({ id, title, userId, completed });
+
+      const returnedTodo = await todoService.postTodo({
+        title,
+        userId,
+        completed,
+      });
+
+      setTodos(currentTodos => [...currentTodos, returnedTodo]);
+    } catch (error) {
+      if (title.length === 0) {
+        applyErrorForSomeTime('Title should not be empty');
+      } else {
+        applyErrorForSomeTime('Unable to add a todo');
+      }
+
+      throw error;
+    } finally {
+      setTempTodo(null);
+    }
+  };
+
+  const updateTodo = (todoToUpdate: Todo) => {
+    return todoService
+      .updateTodo(todoToUpdate)
+      .then(() =>
+        setTodos(currentTodos => {
+          return currentTodos.map(todo =>
+            todo.id === todoToUpdate.id ? todoToUpdate : todo,
+          );
+        }),
+      )
+      .catch(error => {
+        applyErrorForSomeTime('Unable to update a todo');
+        throw error;
+      });
+  };
+
+  const deleteTodo = (todoId: number) => {
+    setProcessingTodosId(currIds => [...currIds, todoId]);
+
+    return todoService
+      .deleteTodo(todoId)
+      .then(() => {
+        setTodos(currentTodos =>
+          currentTodos.filter(todo => todo.id !== todoId),
+        );
+      })
+      .catch(error => {
+        applyErrorForSomeTime('Unable to delete a todo');
+        throw error;
+      })
+      .finally(() => {
+        setProcessingTodosId(currIds => currIds.filter(id => id !== todoId));
+      });
+  };
+
+  return (
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          onAddTodo={addTodo}
+          onUpdateTodosCompleted={updateTodo}
+        />
+
+        <TodoList
+          todos={preparedTodos}
+          processings={processingTodosId}
+          tempTodo={tempTodo}
+          onDeleteTodo={deleteTodo}
+        />
+
+        {todos.length > 0 && (
+          <Footer
+            selectedFilter={selectedFilter}
+            setSelectedFilter={setSelectedFilter}
+            activeTodosCount={activeTodosCount}
+            completedTodosCount={completedTodosCount}
+          />
+        )}
+      </div>
+
+      <ErrorMessage
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      />
+    </div>
   );
 };
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+type Props = {
+  todos: Todo[];
+  processings: number[];
+  tempTodo: Todo | null;
+  onDeleteTodo: (todoId: number) => Promise<void>;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  processings,
+  tempTodo,
+  onDeleteTodo,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      <TransitionGroup>
+        {todos.map(todo => (
+          <CSSTransition key={todo.id} timeout={300} classNames="item">
+            <TodoItem
+              todo={todo}
+              isProcessed={processings.includes(todo.id)}
+              onDelete={onDeleteTodo}
+            />
+          </CSSTransition>
+        ))}
+
+        {tempTodo && (
+          <CSSTransition key={0} timeout={300} classNames="temp-item">
+            <TodoItem todo={tempTodo} isProcessed />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+    </section>
+  );
+};
