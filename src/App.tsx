82dfac15ab6787403292a21fea304bd6e3d9d482
Olexandra Pastushenko <sandorik1608@gmail.com>
2024-09-26 21:06:06 +0300

    add-task-solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { USER_ID, deleteTodos, getTodos } from './api/todos';
+import { Todo } from './types/Todo';
+import { Header } from './components/Header/Header';
+import { TodoList } from './components/TodoList/TodoList';
+import { Footer } from './components/Footer/Footer';
+import { ErrorMessage } from './components/ErrorMessage/ErrorMessage';
+import { TodoStatusFilter } from './types/TodoStatusFilter';
+import { getPreparedTodos } from './utils/getPreparedTodos';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState([0]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [temporaryTodo, setTemporaryTodo] = useState<Todo | null>(null);
+  const [selectedFilter, setSelectedFilter] = useState<TodoStatusFilter>(
+    TodoStatusFilter.All,
+  );
+
+  const activeTodosCount = todos.filter(todo => !todo.completed).length;
+  const completedTodosCount = todos.filter(todo => todo.completed).length;
+  const preparedTodos = getPreparedTodos(todos, selectedFilter);
+
+  const handleDeleteTodo = (todoId: number) => {
+    setLoading(prev => [...prev, todoId]);
+
+    deleteTodos(todoId)
+      .then(() =>
+        setTodos((prev: Todo[]) => prev.filter(todo => todo.id !== todoId)),
+      )
+      .catch(() => {
+        setErrorMessage('Unable to delete a todo');
+      })
+      .finally(() => {
+        setLoading((prev: number[]) => prev.filter(id => id !== todoId));
+      });
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => setErrorMessage('Unable to load todos'));
+  }, []);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          setTodos={setTodos}
+          setTemporaryTodo={setTemporaryTodo}
+          setErrorMessage={setErrorMessage}
+        />
+
+        <TodoList
+          todos={preparedTodos}
+          loading={loading}
+          temporaryTodo={temporaryTodo}
+          handleDeleteTodo={handleDeleteTodo}
+        />
+        {!!todos.length && (
+          <Footer
+            todos={todos}
+            selectedFilter={selectedFilter}
+            activeTodosCount={activeTodosCount}
+            handleDeleteTodo={handleDeleteTodo}
+            setSelectedFilter={setSelectedFilter}
+            completedTodosCount={completedTodosCount}
+          />
+        )}
+      </div>
+
+      <ErrorMessage
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      />
+    </div>
   );
 };
