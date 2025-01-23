1243c1b4d063db0d9774b4747a0317a655378259
Dmytro Moshkivskyi <dmosh@ukr.net>
2025-01-23 11:49:29 +0200

    react_todo-app-add-and-delete by Dimoshua

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,126 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import React, { useEffect, useRef, useState } from 'react';
 
-const USER_ID = 0;
+import * as postService from './api/todos';
+
+import { Errors } from './components/Error';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodosList';
+
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filter';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todosType, setTodosType] = useState<Filter>(Filter.All);
+  const [isError, setIsError] = useState('');
+  const [loadingTodos, setLoadingTodos] = useState<number[]>([]);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleSetError = (message: string) => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    setIsError(message);
+
+    timerRef.current = setTimeout(() => {
+      setIsError('');
+      timerRef.current = null;
+    }, 3000);
+  };
+
+  useEffect(() => {
+    postService
+      .getTodos()
+      .then(setTodos)
+      .catch(() => {
+        handleSetError('Unable to load todos');
+        new Error('Unable to load todos');
+      });
+  }, []);
+
+  const visibleTodos = todos.filter(todo => {
+    if (todosType === Filter.Active) {
+      return !todo.completed;
+    }
+
+    if (todosType === Filter.Completed) {
+      return todo.completed;
+    }
+
+    return true;
+  });
+
+  const handleTodosTypeChange = (type: Filter) => {
+    setTodosType(type);
+  };
+
+  const handleAddTodo = (title: string) => {
+    if (title.length === 0) {
+      handleSetError('Title should not be empty');
+
+      return Promise.reject(new Error('Title is empty'));
+    }
+
+    setTempTodo({ id: 0, title: title, completed: false } as Todo);
+
+    return postService
+      .addTodo(title, false)
+      .then(newTodo => {
+        setTodos(currentList => [...currentList, newTodo]);
+      })
+      .catch(() => {
+        handleSetError('Unable to add a todo');
+        throw new Error('Unable to add a todo');
+      })
+      .finally(() => setTempTodo(null));
+  };
+
+  const handleDeleteTodo = (todoId: number) => {
+    setLoadingTodos(prev => [...prev, todoId]);
+
+    return postService
+      .deleteTodo(todoId)
+      .then(() => {
+        setTodos(currentList =>
+          currentList?.filter(todo => todo.id !== todoId),
+        );
+      })
+      .catch(() => {
+        setIsError('Unable to delete a todo');
+        handleSetError('Unable to delete a todo');
+      })
+      .finally(() => {
+        setLoadingTodos(prev => prev.filter(id => id !== todoId));
+      });
+  };
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header handleAddTodo={handleAddTodo} todos={todos} />
+
+        <TodoList
+          todos={visibleTodos}
+          tempTodo={tempTodo}
+          loadingTodos={loadingTodos}
+          handleDeleteTodo={handleDeleteTodo}
+        />
+
+        <Footer
+          todos={todos}
+          todosType={todosType}
+          handleTodosTypeChange={handleTodosTypeChange}
+          handleDeleteTodo={handleDeleteTodo}
+        />
+      </div>
+
+      <Errors error={isError} />
+    </div>
   );
 };
