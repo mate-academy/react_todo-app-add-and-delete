6208bc7861db710bd3ae42b2c6c6afc74b98f63d
Vitor MF <vitorfreire419@gmail.com>
2025-05-02 10:23:50 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useRef, useState } from 'react';
 import { UserWarning } from './UserWarning';
 import {
@@ -358,6 +356,7 @@ export const App: React.FC = () => {
                 setTodoTitle(todo.title);
               }}
             >
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
               <label className="todo__status-label">
                 <input
                   data-cy="TodoStatus"
@@ -367,7 +366,6 @@ export const App: React.FC = () => {
                   checked={todo.completed}
                 />
               </label>
-
               {editTodoId === todo.id ? (
                 <form
                   ref={formRef}
@@ -408,7 +406,6 @@ export const App: React.FC = () => {
                   </button>
                 </>
               )}
-
               <div
                 data-cy="TodoLoader"
                 className={`modal overlay ${loadingTodoIds.includes(todo.id) ? 'is-active' : 'hidden'}`}
