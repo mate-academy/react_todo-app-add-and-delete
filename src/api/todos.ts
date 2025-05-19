b0e69c4942ac12f9bbc0c2d3fda38f993a1d67da
Mykyta Kucherenko <nikitachucherenko.89@gmail.com>
2025-05-19 15:58:04 +0100

    react_todo-app-add-and-delete

---
+++ src/api/todos.ts
@@ -0,0 +1,25 @@
+import { Todo } from '../types/Todo';
+import { client } from '../helpers/fetchClient';
+
+export const USER_ID = 2181;
+
+export const getTodos = () => {
+  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+};
+
+export const postTodos = async (data: Todo) => {
+  try {
+    const response = await client.post<Todo>('/todos', {
+      ...data,
+      userId: USER_ID,
+    });
+
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
