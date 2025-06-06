632cb715195d67383cd5684d0967b0e521195cf8
Uusuff <90629840+Uusuff@users.noreply.github.com>
2025-06-06 09:33:00 +0200

    add task solution

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -16,67 +16,55 @@ export const TodoList: React.FC<Props> = ({
   tempTodo,
 }) => (
   <section className="todoapp__main" data-cy="TodoList">
-    {todos.map(todo => {
-      const checkboxId = `todo-checkbox-${todo.id}`;
-
-      return (
-        <div
-          key={todo.id}
-          data-cy="Todo"
-          className={`todo ${todo.completed ? 'completed' : ''}`}
-        >
-          <label htmlFor={checkboxId} className="todo__status-label">
-            Mark complete
-          </label>
-
+    {todos.map(todo => (
+      <div
+        key={todo.id}
+        data-cy="Todo"
+        className={`todo ${todo.completed ? 'completed' : ''}`}
+      >
+        <label className="todo__status-label">
+          <span className={classNames('visually-hidden')}>Toggle todo</span>
           <input
-            id={checkboxId}
             data-cy="TodoStatus"
             type="checkbox"
             className="todo__status"
             checked={todo.completed}
             readOnly
           />
+        </label>
 
-          <span data-cy="TodoTitle" className="todo__title">
-            {todo.title}
-          </span>
+        <span data-cy="TodoTitle" className="todo__title">
+          {todo.title}
+        </span>
 
-          <button
-            type="button"
-            className="todo__remove"
-            data-cy="TodoDelete"
-            onClick={() => onDelete(todo.id)}
-            disabled={loadingTodoIds.includes(todo.id)}
-          >
-            ×
-          </button>
+        <button
+          type="button"
+          className="todo__remove"
+          data-cy="TodoDelete"
+          onClick={() => onDelete(todo.id)}
+          disabled={loadingTodoIds.includes(todo.id)}
+        >
+          ×
+        </button>
 
-          <div
-            data-cy="TodoLoader"
-            className={classNames('modal overlay', {
-              'is-active': loadingTodoIds.includes(todo.id),
-            })}
-          >
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
-          </div>
+        <div
+          data-cy="TodoLoader"
+          className={classNames('modal overlay', {
+            'is-active': loadingTodoIds.includes(todo.id),
+          })}
+        >
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
         </div>
-      );
-    })}
+      </div>
+    ))}
 
     {tempTodo && (
       <div key="temp" data-cy="Todo" className="todo">
-        <label htmlFor="temp-checkbox" className="todo__status-label">
-          Mark complete
+        <label className="todo__status-label">
+          <span className={classNames('visually-hidden')}>Toggle todo</span>
+          <input type="checkbox" className="todo__status" disabled />
         </label>
-
-        <input
-          id="temp-checkbox"
-          type="checkbox"
-          className="todo__status"
-          disabled
-        />
 
         <span data-cy="TodoTitle" className="todo__title">
           {tempTodo.title}
