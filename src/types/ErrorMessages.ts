c7413f9f1fb663fa3304a7764c64799a96f15c0b
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-07-27 20:33:18 +0200

    Add files via upload

---
+++ src/types/ErrorMessages.ts
@@ -0,0 +1,8 @@
+export enum ErrorMessages {
+  None = '',
+  OnGet = 'Unable to load todos',
+  OnEmptyTitle = 'Title should not be empty',
+  OnPost = 'Unable to add a todo',
+  OnDelete = 'Unable to delete a todo',
+  OnPatch = 'Unable to update a todo',
+}
