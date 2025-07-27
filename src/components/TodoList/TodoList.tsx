e38c998f3efdfd44a2f2fc91bdd77e125308e19a
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-07-27 20:32:04 +0200

    Update TodoList.tsx

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,40 +1,79 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
-import { TodoItem } from '../TodoItem';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { deleteTodo } from '../../api/todos';
+import { TodoItem } from '../TodoItem/TodoItem';
+import { ErrorMessages } from '../../types/ErrorMessages';
 
-type Props = {
-  todos: Todo[];
+interface Props {
+  todoList: Todo[];
   tempTodo: Todo | null;
-  todoIdsToDelete: number[];
-  onDelete: (id: number) => Promise<void>;
-};
+  deletedTodo: number[];
+  setTodoData: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setDeletedTodo: React.Dispatch<React.SetStateAction<number[]>>;
+  setErrorMessage: (value: ErrorMessages) => void;
+  inputRef: React.RefObject<HTMLInputElement>;
+}
 
 export const TodoList: React.FC<Props> = ({
-  todos,
+  todoList,
   tempTodo,
-  todoIdsToDelete,
-  onDelete,
+  deletedTodo,
+  setTodoData,
+  setDeletedTodo,
+  setErrorMessage,
+  inputRef,
 }) => {
+  const handleDelete = (id: number) => {
+    setDeletedTodo(cur => [...cur, id]);
+
+    deleteTodo(id)
+      .then(() => setTodoData(cur => cur.filter(todo => todo.id !== id)))
+      .catch(() => setErrorMessage(ErrorMessages.OnDelete))
+      .finally(() => {
+        setDeletedTodo(cur => cur.filter(curId => curId !== id));
+        setTimeout(() => {
+          inputRef.current?.focus();
+        }, 0);
+      });
+  };
+
+  const handleSwitchStatus = (currentId: number) => {
+    setTodoData(current =>
+      current.map(todo =>
+        todo.id === currentId ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+
+    // if (false) {
+    //   createErrorMessage(ErrorMessages.OnPatch);
+    // }
+  };
+
+  // const handleUpdate = () => {
+  //   if (false) {
+  //     createErrorMessage(ErrorMessages.OnPatch);
+  //   }
+
+  //   patchTodo();
+  // };
+
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      <TransitionGroup>
-        {todos.map(todo => (
-          <CSSTransition key={todo.id} timeout={300} classNames="item">
-            <TodoItem
-              todo={todo}
-              isLoading={todoIdsToDelete.includes(todo.id)}
-              onDelete={onDelete}
-            />
-          </CSSTransition>
-        ))}
+      {todoList.map((todo: Todo) => {
+        const isOverlayActive = deletedTodo.includes(todo.id);
 
-        {tempTodo && (
-          <CSSTransition timeout={300} classNames="item">
-            <TodoItem todo={tempTodo} isLoading={true} />
-          </CSSTransition>
-        )}
-      </TransitionGroup>
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            isOverlayActive={isOverlayActive}
+            handleDelete={handleDelete}
+            handleSwitchStatus={handleSwitchStatus}
+          />
+        );
+      })}
+
+      {tempTodo && <TodoItem todo={tempTodo} />}
     </section>
   );
 };
