e865c659b26064c1261ae5880ed3871dcb241463
NastyBaster <rsn007@gmail.com>
2026-01-30 20:21:06 +0200

    text field should be focused by default

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from '../types/Todo';
 
 interface Props {
@@ -5,20 +7,41 @@ interface Props {
 }
 
 export const Header: React.FC<Props> = ({ todos }) => {
+    const [title, setTitle] = useState('');
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+   function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+  }
+
+    function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+
   return (
     <header className="todoapp__header">
       <button
         type="button"
-        className={`todoapp__toggle-all ${todos.every(t => t.completed) ? 'active' : ''}`}
+        className={classNames('todoapp__toggle-all', {
+          active: todos.every(todo => todo.completed),
+        })}
         data-cy="ToggleAllButton"
       />
 
-      <form onSubmit={e => e.preventDefault()}>
+      <form onSubmit={handleSubmit}>
         <input
+          ref={inputRef}
           data-cy="NewTodoField"
           type="text"
           className="todoapp__new-todo"
           placeholder="What needs to be done?"
+          value={title}
+          onChange={handleTitleChange}
+          aria-label="New todo title"
         />
       </form>
     </header>
