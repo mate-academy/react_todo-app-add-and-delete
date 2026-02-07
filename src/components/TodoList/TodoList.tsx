c14934f14062177476a47e08dfa5cb7c13be2e69
Sertavr <pampuh.net@gmail.com>
2026-02-07 22:08:23 +0200

    added smooth animations

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TodoItem } from '../TodoItem/TodoItem';
 import { Todo } from '../../types/Todo';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 type Props = {
   prepareTodos: () => Todo[];
@@ -17,24 +18,30 @@ export const TodoList: React.FC<Props> = ({
 }) => {
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {prepareTodos().map(todo => (
-        <TodoItem
-          key={todo.id}
-          title={todo.title}
-          completed={todo.completed}
-          isLoading={false}
-          deletedId={deletedId}
-          id={todo.id}
-          handleDelete={() => handleDelete(todo.id)}
-        />
-      ))}
-      {!!tempTodo && (
-        <TodoItem
-          title={tempTodo.title}
-          completed={tempTodo.completed}
-          isLoading={true}
-        />
-      )}
+      <TransitionGroup>
+        {prepareTodos().map(todo => (
+          <CSSTransition key={todo.id} timeout={300} classNames="item">
+            <TodoItem
+              key={todo.id}
+              title={todo.title}
+              completed={todo.completed}
+              isLoading={false}
+              deletedId={deletedId}
+              id={todo.id}
+              handleDelete={() => handleDelete(todo.id)}
+            />
+          </CSSTransition>
+        ))}
+        {!!tempTodo && (
+          <CSSTransition key={0} timeout={300} classNames="temp-item">
+            <TodoItem
+              title={tempTodo.title}
+              completed={tempTodo.completed}
+              isLoading={true}
+            />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </section>
   );
 };
