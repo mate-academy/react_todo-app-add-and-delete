bcb114cc1a3411b04536afdbaab40beffe363a4e
AnnaaAAA3432 <kykyangelina182@gmail.com>
2026-02-23 15:06:14 +0200

    Solution

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todo: Todo;
+  isTemp?: boolean;
+  isDeleting?: boolean;
+  onDelete?: (id: number) => void;
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  isTemp,
+  isDeleting,
+  onDelete,
+}) => {
+  const showLoader = isTemp || isDeleting;
+
+  return (
+    <div data-cy="Todo" className={todo.completed ? 'todo completed' : 'todo'}>
+      <div className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          readOnly
+        />
+      </div>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      <div
+        data-cy="TodoLoader"
+        className={`todo__loader ${showLoader ? 'is-active' : ''}`}
+      ></div>
+
+      {/* Remove button appears only on hover */}
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDelete"
+        onClick={() => onDelete?.(todo.id)}
+      >
+        ×
+      </button>
+    </div>
+  );
+};
