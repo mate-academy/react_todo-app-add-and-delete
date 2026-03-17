f878cd99139186dd515aaed2398b83b0105bef1b
AnnaAAA3432 <kykyangelina182@gmail.com>
2026-03-17 19:22:16 +0100

    Solution

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -34,8 +34,11 @@ export const TodoItem: React.FC<Props> = ({
 
       <div
         data-cy="TodoLoader"
-        className={`todo__loader ${showLoader ? 'is-active' : ''}`}
-      ></div>
+        className={`todo__loader modal overlay ${showLoader ? 'is-active' : ''}`}
+      >
+          <div className='modal-background has-background-white-ter'></div>
+           <div className='loader'></div>
+      </div>
 
       {/* Remove button appears only on hover */}
       <button
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -21,7 +21,10 @@ export const TodoList: React.FC<Props> = ({
   return (
     <section className="todoapp__main" data-cy="TodoList">
       {/* This is a completed todo */}
-      {isLoading && <div data-cy="TodoLoader" className="loader"></div>}
+      {isLoading && <div data-cy="TodoLoader" className="modal overlay">
+          <div className='loader'></div>
+          <div className='modal-background has-background-white-ter'></div>
+        </div>}
 
       {!isLoading && (
         <TransitionGroup>
