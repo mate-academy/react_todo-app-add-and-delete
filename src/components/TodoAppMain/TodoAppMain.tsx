ebc474db928362e0f3d238ab1e890dee11c62424
Bohdan <bodiiiiic@gmail.com>
2026-04-25 11:01:23 +0100

    sol

--- src/components/TodoAppMain/TodoAppMain.tsx
+++ src/components/TodoAppMain/TodoAppMain.tsx
@@ -8,66 +8,82 @@ type Props = {
   todos: Todo[];
   tempTodo: Todo | null;
   loaderClearButton: boolean;
-  onSelectedTodo : (todoId : number) => void
+  onSelectedTodo: (todoId: number) => void;
 };
 
-export const TodoAppMain = React.memo<Props>(({ loaderClearButton,
-   selectedDeleteTodo, loaderDelete, todos, tempTodo, onSelectedTodo}) => {
-  const clickButton = useCallback(
-    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, todoId: number) => {
-      event.preventDefault();
-      onSelectedTodo(todoId);
-  },[])
-  return (
-    <section className="todoapp__main" data-cy="TodoList">
-      {todos.map(todo => (
-        <div
-          key={todo.id}
-          data-cy="Todo"
-          className={classNames('todo item-enter-item', {
-            completed: todo.completed,
-          })}
-        >
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label className="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              className="todo__status"
-              checked={todo.completed}
-              readOnly
-            />
-          </label>
+export const TodoAppMain = React.memo<Props>(
+  ({
+    loaderClearButton,
+    selectedDeleteTodo,
+    loaderDelete,
+    todos,
+    tempTodo,
+    onSelectedTodo,
+  }) => {
+    const clickButton = useCallback(
+      (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+        todoId: number,
+      ) => {
+        event.preventDefault();
+        onSelectedTodo(todoId);
+      },
+      [],
+    );
 
-          <span data-cy="TodoTitle" className="todo__title">
-            {todo.title}
-          </span>
-
-          {/* Remove button appears only on hover */}
-          <button 
-          type="button" 
-          className="todo__remove" 
-          data-cy="TodoDelete"
-          onClick={event => clickButton(event, todo.id)}>
-            ×
-          </button>
-
-          {/* overlay will cover the todo while it is being deleted or updated */}
+    return (
+      <section className="todoapp__main" data-cy="TodoList">
+        {todos.map(todo => (
           <div
-            data-cy="TodoLoader"
-            className={classNames('modal overlay ', {
-              'is-active': todo.title === tempTodo?.title 
-                || (loaderDelete && todo.id === selectedDeleteTodo)
-                || (loaderClearButton && todo.completed),
+            key={todo.id}
+            data-cy="Todo"
+            className={classNames('todo item-enter-item', {
+              completed: todo.completed,
             })}
           >
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label className="todo__status-label">
+              <input
+                data-cy="TodoStatus"
+                type="checkbox"
+                className="todo__status"
+                checked={todo.completed}
+                readOnly
+              />
+            </label>
+
+            <span data-cy="TodoTitle" className="todo__title">
+              {todo.title}
+            </span>
+
+            {/* Remove button appears only on hover */}
+            <button
+              type="button"
+              className="todo__remove"
+              data-cy="TodoDelete"
+              onClick={event => clickButton(event, todo.id)}
+            >
+              ×
+            </button>
+
+            {/* overlay will cover the todo while it is being deleted or updated */}
+            <div
+              data-cy="TodoLoader"
+              className={classNames('modal overlay ', {
+                'is-active':
+                  todo.title === tempTodo?.title ||
+                  (loaderDelete && todo.id === selectedDeleteTodo) ||
+                  (loaderClearButton && todo.completed),
+              })}
+            >
+              <div className="modal-background has-background-white-ter" />
+              <div className="loader" />
+            </div>
           </div>
-        </div>
-      ))}
-    </section>
-  );
-});
+        ))}
+      </section>
+    );
+  },
+);
 
 TodoAppMain.displayName = 'TodoAppMain';
