19623dfdec9b68d78547a9dad2e62402c1eae65d
Denys Boiko <big4ever6996@gmail.com>
2026-05-16 18:50:17 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,195 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { USER_ID, getTodos, addTodo, deleteTodo } from './api/todos';
+import { Todo } from './types/Todo';
+import { Status } from './types/Status';
+import { TodoList } from './components/TodoList';
+import { Filter } from './components/Filter';
+import classNames from 'classnames';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Status>(Status.All);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [title, setTitle] = useState('');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [processingIds, setProcessingIds] = useState<number[]>([]);
+
+  const titleField = useRef<HTMLInputElement>(null);
+
+  const showError = (message: string) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage('');
+    }, 3000);
+  };
+
+  useEffect(() => {
+    if (!USER_ID) {
+      return;
+    }
+
+    getTodos()
+      .then(setTodos)
+      .catch(() => showError('Unable to load todos'));
+  }, []);
+
+  useEffect(() => {
+    if (titleField.current) {
+      titleField.current.focus();
+    }
+  }, [todos.length]);
+
+  const handleAddTodo = (e: React.FormEvent) => {
+    e.preventDefault();
+    const normalizedTitle = title.trim();
+
+    if (!normalizedTitle) {
+      showError('Title should not be empty');
+
+      return;
+    }
+
+    setTempTodo({
+      id: 0,
+      title: normalizedTitle,
+      completed: false,
+      userId: USER_ID,
+    });
+
+    addTodo(normalizedTitle)
+      .then(newTodo => {
+        setTodos(currentTodos => [...currentTodos, newTodo]);
+        setTitle('');
+      })
+      .catch(() => {
+        showError('Unable to add a todo');
+      })
+      .finally(() => {
+        setTempTodo(null);
+        setTimeout(() => {
+          titleField.current?.focus();
+        }, 0);
+      });
+  };
+
+  const handleDeleteTodo = (todoId: number) => {
+    setProcessingIds(current => [...current, todoId]);
+
+    deleteTodo(todoId)
+      .then(() => {
+        setTodos(current => current.filter(todo => todo.id !== todoId));
+      })
+      .catch(() => {
+        showError('Unable to delete a todo');
+      })
+      .finally(() => {
+        setProcessingIds(current => current.filter(id => id !== todoId));
+      });
+  };
+
+  const handleClearCompleted = () => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    completedTodos.forEach(todo => handleDeleteTodo(todo.id));
+  };
+
+  const visibleTodos = todos.filter(todo => {
+    switch (filter) {
+      case Status.Active:
+        return !todo.completed;
+      case Status.Completed:
+        return todo.completed;
+      case Status.All:
+      default:
+        return true;
+    }
+  });
+
+  const activeTodosCount = todos.filter(todo => !todo.completed).length;
+  const hasCompletedTodos = todos.some(todo => todo.completed);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          <button
+            type="button"
+            className={classNames('todoapp__toggle-all', {
+              active: activeTodosCount === 0 && todos.length > 0,
+            })}
+            data-cy="ToggleAllButton"
+          />
+
+          <form onSubmit={handleAddTodo}>
+            <input
+              data-cy="NewTodoField"
+              type="text"
+              className="todoapp__new-todo"
+              placeholder="What needs to be done?"
+              ref={titleField}
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              disabled={tempTodo !== null}
+            />
+          </form>
+        </header>
+
+        {(todos.length > 0 || tempTodo !== null) && (
+          <TodoList
+            todos={visibleTodos}
+            tempTodo={tempTodo}
+            processingIds={processingIds}
+            onDelete={handleDeleteTodo}
+          />
+        )}
+
+        {todos.length > 0 && (
+          <footer className="todoapp__footer" data-cy="Footer">
+            <span className="todo-count" data-cy="TodosCounter">
+              {activeTodosCount} items left
+            </span>
+
+            <Filter filter={filter} onFilterChange={setFilter} />
+
+            <button
+              type="button"
+              className="todoapp__clear-completed"
+              data-cy="ClearCompletedButton"
+              disabled={!hasCompletedTodos}
+              onClick={handleClearCompleted}
+            >
+              Clear completed
+            </button>
+          </footer>
+        )}
+      </div>
+
+      <div
+        data-cy="ErrorNotification"
+        className={classNames(
+          'notification is-danger is-light has-text-weight-normal',
+          {
+            hidden: !errorMessage,
+          },
+        )}
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={() => setErrorMessage('')}
+        />
+        {errorMessage}
+      </div>
+    </div>
   );
 };
