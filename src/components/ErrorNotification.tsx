f0e4d46fcb1dc529de526b06ca80ddb419abfd57
Yana <utrsach@gmail.com>
2026-06-17 16:24:39 +0300

    Solution

---
+++ src/components/ErrorNotification.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type Prop = {
+  errorMessage: string;
+  setErrorMessage: (errorMessage: string) => void;
+};
+
+export const ErrorNotification: React.FC<Prop> = ({
+  errorMessage,
+  setErrorMessage,
+}) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={`notification is-danger is-light has-text-weight-normal ${!errorMessage ? 'hidden' : ''}`}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setErrorMessage('')}
+      />
+      {/* show only one message at a time */}
+      {errorMessage}
+    </div>
+  );
+};
---
+++ src/components/Footer.tsx
@@ -0,0 +1,54 @@
+import { Todo } from '../types/Todo';
+import React from 'react';
+
+type Prop = {
+  todos: Todo[];
+  filter: string;
+  setFilter: (filter: string) => void;
+  onClear: () => void;
+};
+
+export const Footer: React.FC<Prop> = ({
+  todos,
+  filter,
+  setFilter,
+  onClear,
+}) => {
+  return (
+    <>
+      <footer className="todoapp__footer" data-cy="Footer">
+        <span className="todo-count" data-cy="TodosCounter">
+          {todos.filter((todo: Todo) => !todo.completed).length} items left
+        </span>
+
+        {/* Active link should have the 'selected' class */}
+        <nav className="filter" data-cy="Filter">
+          {['All', 'Active', 'Completed'].map(filterName => {
+            return (
+              <a
+                href="#/"
+                className={`filter__link ${filter === filterName ? 'selected' : ''}`}
+                data-cy={`FilterLink${filterName}`}
+                key={filterName}
+                onClick={() => setFilter(filterName)}
+              >
+                {filterName}
+              </a>
+            );
+          })}
+        </nav>
+
+        {/* this button should be disabled if there are no completed todos */}
+        <button
+          type="button"
+          className="todoapp__clear-completed"
+          data-cy="ClearCompletedButton"
+          disabled={!todos.some(todo => todo.completed)}
+          onClick={onClear}
+        >
+          Clear completed
+        </button>
+      </footer>
+    </>
+  );
+};
