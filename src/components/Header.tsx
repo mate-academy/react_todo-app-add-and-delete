aa77a0dbc2ea1ec0e6f3290fcf26edb2e11a8129
Timur K <timurkakandzo@gmail.com>
2026-07-25 09:08:41 +0300

    final2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { ErrorMessage } from '../types/ErrorMessage';
-import classNames from 'classnames';
 
 interface Props {
   todosCount: number;
@@ -65,9 +64,7 @@ export const Header: React.FC<Props> = ({
           autoFocus
           data-cy="NewTodoField"
           type="text"
-          className={classNames('todoapp__toggle-all', {
-            active: isAllCompleted,
-          })}
+          className="todoapp__new-todo"
           placeholder="What needs to be done?"
           value={title}
           onChange={e => setTitle(e.target.value)}
