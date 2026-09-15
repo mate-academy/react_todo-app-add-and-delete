e8f792d7666a081920722587a673cb5e6264ee3e
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-09-15 14:52:54 +0300

    changed isLoading checking logic

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -6,7 +6,7 @@ export const Loader: React.FC<Props> = ({ isLoading }) => {
   return (
     <div
       data-cy="TodoLoader"
-      className={`modal overlay ${isLoading ? 'is-hidden' : ''}`}
+      className={`modal overlay ${isLoading ? 'is-hidden' : 'hidden'}`}
     >
       <div className="modal-background has-background-white-ter" />
       <div className="loader" />
